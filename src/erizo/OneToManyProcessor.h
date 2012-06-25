defb66cdf7ed2e117f9cf1d7342ec518e3b930e3
Pedro Rodriguez <prodriguez@dit.upm.es>
2012-06-25 15:26:16 +0200

    Documentation

--- src/erizo/OneToManyProcessor.h
+++ src/erizo/OneToManyProcessor.h
@@ -13,12 +13,29 @@ namespace erizo{
 
 class WebRtcConnection;
 
+/**
+ * Represents a One to Many connection.
+ * Receives media from one publisher and retransmits it to every subscriber.
+ */
 class OneToManyProcessor : public MediaReceiver {
 public:
 	OneToManyProcessor();
 	virtual ~OneToManyProcessor();
+	/**
+	 * Sets the Publisher
+	 * @param webRtcConn The WebRtcConnection of the Publisher
+	 */
 	void setPublisher(WebRtcConnection* webRtcConn);
+	/**
+	 * Sets the subscriber
+	 * @param webRtcConn The WebRtcConnection of the subscriber
+	 * @param peerId An unique Id for the subscriber
+	 */
 	void addSubscriber(WebRtcConnection* webRtcConn, int peerId);
+	/**
+	 * Eliminates the subscriber given its peer id
+	 * @param peerId the peerId
+	 */
 	void removeSubscriber(int peerId);
 	int receiveAudioData(char* buf, int len);
 	int receiveVideoData(char* buf, int len);
