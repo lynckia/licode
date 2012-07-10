bac8a12c408bb48ff13c0d35f15e41914a32b886
Álvaro Alonso <aalonsog@dit.upm.es>
2012-07-10 17:45:37 +0200

    Added Client Communication class

---
+++ erizoClient/L.Comm.js
@@ -0,0 +1,14 @@
+var L = L || {};
+L.Comm = function(L) {
+	var privateVar;
+	var connect = function(token, callback, error) {
+		L.Comm.privateVar;
+	};
+	var sendMessage = function(type, msg, callback, error) {
+
+	};
+	return {
+		connect: connect,
+		sendMessage: sendMessage
+	};
+}(L);
