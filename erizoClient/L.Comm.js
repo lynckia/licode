7d6dfb40769cfdd36c8f2e2cea9970253df89e5c
Álvaro Alonso <aalonsog@dit.upm.es>
2012-07-11 12:32:52 +0200

    Changes about socket.io

--- erizoClient/L.Comm.js
+++ erizoClient/L.Comm.js
@@ -1,14 +1,26 @@
 var L = L || {};
+
 L.Comm = function(L) {
-	var privateVar;
+
+	var socket;
+	var server = 'rosendo.dit.upm.es:8080';
+
 	var connect = function(token, callback, error) {
-		L.Comm.privateVar;
+		
+		L.Comm.socket = io.connect(server);
+    	L.Comm.sendMessage('token', token, callback, error);
+    	
 	};
+
 	var sendMessage = function(type, msg, callback, error) {
 
+        L.Comm.socket.emit(type, msg, callback, error);
+
 	};
+
 	return {
 		connect: connect,
 		sendMessage: sendMessage
 	};
+
 }(L);
