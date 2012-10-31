8809241ce94728209456f7213f325c479748847d
Álvaro Alonso <aalonsog@dit.upm.es>
2012-10-31 13:08:53 +0100

    Added examples for bowser

---
+++ test/bowserServer.js
@@ -0,0 +1,51 @@
+var express = require ('express');
+var net = require('net');
+var app = express.createServer();
+var io = require('socket.io').listen(app);
+
+io.set('log level', 1);
+
+app.configure(function () {
+    app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+    app.use(express.logger());
+    app.use(express.static(__dirname + '/public'));
+
+});
+
+app.listen (3004);
+
+var callerId;
+var answererId;
+
+
+io.sockets.on('connection', function (socket) {
+
+    console.log("Socket connect ", socket.id);
+    
+    socket.on('initConnection', function (type) {
+
+
+
+        if (type == 'caller') {
+            callerId = socket.id;
+        } else {
+            answererId = socket.id;    
+        }
+
+        console.log('*************************', type);
+             
+    });
+
+    socket.on('offer', function (mess) {
+        console.log('offer', mess);
+        io.sockets.socket(answererId).emit('signaling', mess);
+    });
+
+    socket.on('answer', function (mess) {
+        console.log('answer', mess);
+        io.sockets.socket(callerId).emit('signaling', mess);
+    });
+
+
+    
+});
