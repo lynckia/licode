1b972c68434926e50e51b29c8d0630a9affeefd1
Álvaro Alonso <aalonsog@dit.upm.es>
2012-11-21 15:52:15 +0100

    Updated gitignore

--- nuveClient/tools/compile.sh
+++ nuveClient/tools/compile.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
+mkdir ../dist
+mkdir ../build
+
 java -jar compiler.jar --js ../src/hmac-sha1.js --js ../src/N.js --js ../src/N.Base64.js --js ../src/N.API.js --js_output_file ../build/nuve.js
