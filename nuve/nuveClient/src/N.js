2f990686e15917c7406b18d3ff1279e183eb261a
Javier Cerviño <jcervino@dit.upm.es>
2012-11-29 11:59:19 +0100

    I clean the gh-pages branch

--- nuve/nuveClient/src/N.js
+++
@@ -1,5 +0,0 @@
-var N = N || {};
-
-N.authors = ['aalonsog@dit.upm.es', 'prodriguez@dit.upm.es','jcervino@dit.upm.es'];
-
-N.version = 0.1;
--- nuve/nuveClient/tools/compile.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-mkdir ../dist
-mkdir ../build
-
-java -jar compiler.jar --js ../src/hmac-sha1.js --js ../src/N.js --js ../src/N.Base64.js --js ../src/N.API.js --js_output_file ../build/nuve.js
