110c2569b9ae3d9f6fe0387846816de266cbd110
Javier Cerviño <jcervino@dit.upm.es>
2012-12-05 10:43:59 +0100

    Testing Travis

--- testacular.conf.js
+++ testacular.conf.js
@@ -55,7 +55,7 @@ autoWatch = true;
 // - PhantomJS
 // - IE (only Windows)
 // CLI --browsers Chrome,Firefox,Safari
-browsers = [Chrome];
+browsers = ["Chrome"];
 
 // If browser does not capture in given timeout [ms], kill it
 // CLI --capture-timeout 5000
