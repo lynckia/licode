0ee5c236636cfb139e1faed39b40d7cd7a027759
Pedro Rodriguez <prodriguez@dit.upm.es>
2012-12-12 16:38:24 +0100

    Added Travis fles and deleted old files

---
+++ test/lynckia-test.js
@@ -0,0 +1,18 @@
+/*global require, module*/
+var vows = require('vows'),
+    assert = require('assert'),
+    N = require('../extras/basic_example/nuve'),
+    config = require('../lynckia_config');
+
+vows.describe('lynckia').addBatch({
+    "Lynckia service": {
+        topic: function () {
+            "use strict";
+            return null;
+        },
+        "should have the correct methods defined": function () {
+            "use strict";
+            assert.isTrue(true);
+        }
+    }
+}).export(module);
---
+++ test/nuve-test.js
@@ -0,0 +1,148 @@
+/*global describe, beforeEach, it, XMLHttpRequest, jasmine, expect, waitsFor, runs, Erizo*/
+describe('server', function () {
+    "use strict";
+    var room, createToken, token, localStream, remoteStream;
+
+    createToken = function (userName, role, callback) {
+
+        var req = new XMLHttpRequest(),
+            serverUrl = "http://localhost:3001/",
+            url = serverUrl + 'createToken/',
+            body = {username: userName, role: role};
+
+        req.onreadystatechange = function () {
+            if (req.readyState === 4) {
+                callback(req.responseText);
+            }
+        };
+
+        req.open('POST', url, true);
+        req.setRequestHeader('Content-Type', 'application/json');
+        req.send(JSON.stringify(body));
+    };
+
+    beforeEach(function () {
+    });
+
+    it('should get token', function () {
+        var callback = jasmine.createSpy("token");
+
+        createToken("user", "role", function(_token) {
+            callback();
+            token = _token;
+        });
+
+        waitsFor(function () {
+            return callback.callCount > 0;
+        });
+
+        runs(function () {
+            expect(callback).toHaveBeenCalled();
+        });
+    });
+
+    it('should get access to user media', function () {
+        var callback = jasmine.createSpy("getusermedia");
+
+        localStream = Erizo.Stream({audio: true, video: true, data: true});
+
+        localStream.addEventListener("access-accepted", callback);
+
+        localStream.init();
+
+        waitsFor(function () {
+            return callback.callCount > 0;
+        });
+
+        runs(function () {
+
+            expect(callback).toHaveBeenCalled();
+        });
+    });
+
+    it('should connect to room', function () {
+        var callback = jasmine.createSpy("connectroom");
+
+        room = Erizo.Room({token: token});
+
+        room.addEventListener("room-connected", callback);
+
+        room.connect();
+
+        waitsFor(function () {
+            return callback.callCount > 0;
+        });
+
+        runs(function () {
+            expect(callback).toHaveBeenCalled();
+        });
+    });
+
+    it('should publish stream in room', function () {
+        var callback = jasmine.createSpy("publishroom");
+
+        room.publish(localStream);
+
+        room.addEventListener("stream-added", function(msg) {
+            remoteStream = msg.stream;
+            callback();
+        });
+
+        waitsFor(function () {
+            return callback.callCount > 0;
+        });
+
+        runs(function () {
+            expect(callback).toHaveBeenCalled();
+        });
+    });
+
+    it('should subscribe to stream in room', function () {
+        var callback = jasmine.createSpy("publishroom");
+
+        room.addEventListener("stream-subscribed", function() {
+            callback();
+        });
+
+        room.subscribe(remoteStream);
+
+        waitsFor(function () {
+            return callback.callCount > 0;
+        });
+
+        runs(function () {
+            expect(callback).toHaveBeenCalled();
+        });
+    });
+
+    it('should allow showing a subscribed stream', function () {
+        var divg = document.createElement("div");
+        divg.setAttribute("id", "myDiv");
+        document.body.appendChild(divg);
+
+        remoteStream.show('myDiv');
+
+        waits(500);
+
+        runs(function () {
+            var video = document.getElementById('stream' + remoteStream.getID());
+            expect(video.getAttribute("url")).toBeDefined();
+        });
+    });
+
+    it('should disconnect from room', function () {
+        var callback = jasmine.createSpy("connectroom");
+
+        room.addEventListener("room-disconnected", callback);
+
+        room.disconnect();
+
+        waitsFor(function () {
+            return callback.callCount > 0;
+        });
+
+        runs(function () {
+            expect(callback).toHaveBeenCalled();
+        });
+    });
+});
