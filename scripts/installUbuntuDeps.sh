4af4bfcc3c99ea5ccf3329cee1d90b7812af00c4
Álvaro Alonso <aalonsog@dit.upm.es>
2012-12-12 13:08:34 +0100

    Fixed a bug in install deps script

--- scripts/installUbuntuDeps.sh
+++ scripts/installUbuntuDeps.sh
@@ -7,7 +7,7 @@ pause() {
 
 install_apt_deps(){
   sudo apt-get update
-  sudo apt-get install git make gcc libssl-dev cmake libsrtp0-dev libsrtp0 libnice10 libnice-dev libglib2.0-dev pkg-config nodejs nodejs-dev npm libboost-regex-dev libboost-thread-dev libboost-system-dev rabbitmq-server mongodb openjdk-6-jre
+  sudo apt-get install git make gcc g++ libssl-dev cmake libsrtp0-dev libsrtp0 libnice10 libnice-dev libglib2.0-dev pkg-config nodejs nodejs-dev npm libboost-regex-dev libboost-thread-dev libboost-system-dev rabbitmq-server mongodb openjdk-6-jre
 }
 
 pause "Installing deps via apt-get... [press Enter]"
