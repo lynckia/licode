c2b2e41b027988ef77f88b2d9f93e8d10f2ba0b4
Javier Cervino <jcervino@dit.upm.es>
2013-01-11 16:13:48 +0100

    Updated to new versions of Chrome and Chrome Canary

--- erizo_controller/erizoClient/src/Connection.js
+++ erizo_controller/erizoClient/src/Connection.js
@@ -16,12 +16,14 @@ Erizo.Connection = function (spec) {
     if (typeof module !== 'undefined' && module.exports) {
         L.Logger.error('Publish/subscribe video/audio streams not supported in erizofc yet');
         that = Erizo.FcStack(spec);
-    } else if (window.navigator.appVersion.match(/Chrome\/([\w\W]*?)\./)[1] === "23") {
+    } else if (window.navigator.appVersion.match(/Chrome\/([\w\W]*?)\./)[1] === "23" || 
+               window.navigator.appVersion.match(/Chrome\/([\w\W]*?)\./)[1] === "24") {
         // Google Chrome Stable.
         console.log("Stable!");
         that = Erizo.ChromeStableStack(spec);
         that.browser = "chrome-stable";
-    } else if (window.navigator.appVersion.match(/Chrome\/([\w\W]*?)\./)[1] === "26") {
+    } else if (window.navigator.appVersion.match(/Chrome\/([\w\W]*?)\./)[1] === "25" ||
+               window.navigator.appVersion.match(/Chrome\/([\w\W]*?)\./)[1] === "26") {
         // Google Chrome Canary.
         console.log("Canary!");
         that = Erizo.ChromeCanaryStack(spec);
