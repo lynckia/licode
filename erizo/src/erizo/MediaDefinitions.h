2c5a6349be2287a22b2f28d3b2f3a40950f25e11
Pedro Rodriguez <prodriguez@dit.upm.es>
2013-02-07 17:57:36 +0100

    Added new definitions

--- erizo/src/erizo/MediaDefinitions.h
+++ erizo/src/erizo/MediaDefinitions.h
@@ -18,11 +18,31 @@ struct packet{
  * A MediaReceiver is any class that can receive audio or video data.
  */
 class MediaReceiver{
+protected:
+  unsigned int receiverSSRC_;
 public:
 	virtual int receiveAudioData(char* buf, int len)=0;
 	virtual int receiveVideoData(char* buf, int len)=0;
+  virtual void close()=0;
 	virtual ~MediaReceiver(){};
 };
+
+/**
+ * A MediaSource is any class that produces audio or video data.
+ */
+class MediaSource{
+protected: 
+    unsigned int sourceSSRC_;
+public:
+  virtual void setAudioReceiver(MediaReceiver* audioReceiver)=0;
+  virtual void setVideoReceiver(MediaReceiver* videoReceiver)=0;
+  virtual int receiveRTCP(char* buf, int len)=0;
+  virtual int sendFirPacket();
+  virtual void close()=0;
+	virtual ~MediaSource(){};
+};
+
+
 /**
  * A NiceReceiver is any class that can receive data from a nice connection.
  */
