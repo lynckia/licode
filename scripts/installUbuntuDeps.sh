c71adadd17636ee592f2e8b6b308607884a5d61e
Álvaro Alonso <aalonsog@dit.upm.es>
2013-03-07 19:01:05 +0100

    Updated Lynckia for compatibility with new mongodb npm module version

--- scripts/installUbuntuDeps.sh
+++ scripts/installUbuntuDeps.sh
@@ -6,6 +6,7 @@ pause() {
 }
 
 install_apt_deps(){
+  sudo add-apt-repository ppa:chris-lea/node.js
   sudo apt-get update
   sudo apt-get install git make gcc g++ libssl-dev cmake libnice10 libnice-dev libglib2.0-dev pkg-config nodejs nodejs-dev npm libboost-regex-dev libboost-thread-dev libboost-system-dev rabbitmq-server mongodb openjdk-6-jre
 }
