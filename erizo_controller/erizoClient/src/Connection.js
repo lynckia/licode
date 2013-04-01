12f2b6d927c0136c408c7b1a82b0a1f12eda4f98
Álvaro Alonso <aalonsog@dit.upm.es>
2013-04-01 15:05:05 +0200

    Updated chrome version

--- erizo_controller/erizoClient/src/Connection.js
+++ erizo_controller/erizoClient/src/Connection.js
@@ -16,14 +16,14 @@ Erizo.Connection = function (spec) {
     if (typeof module !== 'undefined' && module.exports) {
         L.Logger.error('Publish/subscribe video/audio streams not supported in erizofc yet');
         that = Erizo.FcStack(spec);
-    } else if (window.navigator.appVersion.match(/Chrome\/([\w\W]*?)\./)[1] === "24" || 
-               window.navigator.appVersion.match(/Chrome\/([\w\W]*?)\./)[1] === "25") {
+    } else if (window.navigator.appVersion.match(/Chrome\/([\w\W]*?)\./)[1] === "25" || 
+               window.navigator.appVersion.match(/Chrome\/([\w\W]*?)\./)[1] === "26") {
         // Google Chrome Stable.
         console.log("Stable!");
         that = Erizo.ChromeStableStack(spec);
         that.browser = "chrome-stable";
-    } else if (window.navigator.appVersion.match(/Chrome\/([\w\W]*?)\./)[1] === "26" ||
-               window.navigator.appVersion.match(/Chrome\/([\w\W]*?)\./)[1] === "27") {
+    } else if (window.navigator.appVersion.match(/Chrome\/([\w\W]*?)\./)[1] === "27" ||
+               window.navigator.appVersion.match(/Chrome\/([\w\W]*?)\./)[1] === "28") {
         // Google Chrome Canary.
         console.log("Canary!");
         that = Erizo.ChromeCanaryStack(spec);
@@ -54,7 +54,7 @@ Erizo.GetUserMedia = function (config, callback, error) {
             console.log('GetUserMedia BOWSER');
         } catch (e) {
             navigator.webkitGetUserMedia(config, callback, error);
-            console.log('GetUserMedia CHROME');
+            console.log('GetUserMedia CHROME', config);
         }
     }
     
