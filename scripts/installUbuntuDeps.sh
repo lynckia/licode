a93a8317db9fba69dd1a119571a30cdb5f450145
Pedro Rodriguez <prodriguez@dit.upm.es>
2013-06-18 16:28:44 +0200

    erizoAPI can now be build with node-gyp

--- scripts/installUbuntuDeps.sh
+++ scripts/installUbuntuDeps.sh
@@ -11,6 +11,7 @@ install_apt_deps(){
   sudo add-apt-repository ppa:chris-lea/node.js-legacy
   sudo apt-get update
   sudo apt-get install git make gcc g++ libssl-dev cmake libnice10 libnice-dev libglib2.0-dev pkg-config nodejs nodejs-dev npm libboost-regex-dev libboost-thread-dev libboost-system-dev rabbitmq-server mongodb openjdk-6-jre
+  sudo npm install -g node-gyp
 }
 
 pause "Installing deps via apt-get... [press Enter]"
