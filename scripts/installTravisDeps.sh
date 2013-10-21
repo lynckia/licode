20b4eb286b84e7663ffa8c130a1f7096c6005d5c
Javier Cerviño <jcervino@dit.upm.es>
2013-10-21 23:57:19 +0200

    Added script and changed travis status logo

---
+++ scripts/installTravisDeps.sh
@@ -0,0 +1,120 @@
+#!/bin/bash
+SCRIPT=`pwd`/$0
+FILENAME=`basename $SCRIPT`
+PATHNAME=`dirname $SCRIPT`
+ROOT=$PATHNAME/..
+BUILD_DIR=$ROOT/build
+CURRENT_DIR=`pwd`
+
+LIB_DIR=$BUILD_DIR/libdeps
+PREFIX_DIR=$LIB_DIR/build/
+
+pause() {
+  read -p "$*"
+}
+
+parse_arguments(){
+  while [ "$1" != "" ]; do
+    case $1 in
+      "--enable-gpl")
+        ENABLE_GPL=true
+        ;;
+    esac
+    shift
+  done
+}
+
+install_apt_deps(){
+  sudo apt-get install -qq make gcc libssl-dev cmake libsrtp0-dev libsrtp0 libnice10 libnice-dev libglib2.0-dev pkg-config libboost-regex-dev libboost-thread-dev libboost-system-dev liblog4cxx10-dev curl openjdk-6-jre
+  sudo npm install -g node-gyp
+  sudo chown -R `whoami` ~/.npm ~/tmp/
+}
+
+install_openssl(){
+  if [ -d $LIB_DIR ]; then
+    cd $LIB_DIR
+    curl -O http://www.openssl.org/source/openssl-1.0.1e.tar.gz
+    tar -zxvf openssl-1.0.1e.tar.gz
+    cd openssl-1.0.1e
+    ./config --prefix=$PREFIX_DIR -fPIC
+    make
+    sudo make install
+    cd $CURRENT_DIR
+  else
+    mkdir -p $LIB_DIR
+    install_openssl
+  fi
+}
+
+install_libnice(){
+  if [ -d $LIB_DIR ]; then
+    cd $LIB_DIR
+    curl -O http://nice.freedesktop.org/releases/libnice-0.1.4.tar.gz
+    tar -zxvf libnice-0.1.4.tar.gz
+    cd libnice-0.1.4
+    ./configure --prefix=$PREFIX_DIR
+    make
+    sudo make install
+    cd $CURRENT_DIR
+  else
+    mkdir -p $LIB_DIR
+    install_libnice
+  fi
+}
+
+install_mediadeps(){
+  sudo apt-get install yasm libvpx. libx264.
+  if [ -d $LIB_DIR ]; then
+    cd $LIB_DIR
+    curl -O https://www.libav.org/releases/libav-9.9.tar.gz
+    tar -zxvf libav-9.9.tar.gz
+    cd libav-9.9
+    ./configure --prefix=$PREFIX_DIR --enable-shared --enable-gpl --enable-libvpx --enable-libx264
+    make
+    sudo make install
+    cd $CURRENT_DIR
+  else
+    mkdir -p $LIB_DIR
+    install_mediadeps
+  fi
+
+}
+
+install_mediadeps_nogpl(){
+  sudo apt-get install yasm libvpx.
+  if [ -d $LIB_DIR ]; then
+    cd $LIB_DIR
+    curl -O https://www.libav.org/releases/libav-9.9.tar.gz
+    tar -zxvf libav-9.9.tar.gz
+    cd libav-9.9
+    ./configure --prefix=$PREFIX_DIR --enable-shared --enable-libvpx
+    make
+    sudo make install
+    cd $CURRENT_DIR
+  else
+    mkdir -p $LIB_DIR
+    install_mediadeps_nogpl
+  fi
+}
+
+
+parse_arguments $*
+
+mkdir -p $PREFIX_DIR
+
+pause "Installing deps via apt-get... [press Enter]"
+install_apt_deps
+
+pause "Installing openssl library...  [press Enter]"
+install_openssl
+
+pause "Installing libnice library...  [press Enter]"
+install_libnice
+
+if [ "$ENABLE_GPL" = "true" ]; then
+  pause "GPL libraries enabled"
+  install_mediadeps
+else
+  pause "No GPL libraries enabled, this disables h264 transcoding, to enable gpl please use the --enable-gpl option"
+  install_mediadeps_nogpl
+fi
