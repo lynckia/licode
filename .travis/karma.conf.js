75ba314a0a1072fb9b31075567c1c9fb36023889
Javier Cerviño <jcervino@dit.upm.es>
2013-10-25 00:03:53 +0200

    Changed from testacular to karma

---
+++ .travis/karma.conf.js
@@ -0,0 +1,75 @@
+// Karma configuration
+// Generated on Thu Oct 24 2013 23:47:37 GMT+0200 (CEST)
+
+module.exports = function(config) {
+  config.set({
+
+    // base path, that will be used to resolve files and exclude
+    basePath: '',
+
+
+    // frameworks to use
+    frameworks: ['jasmine'],
+
+
+    // list of files / patterns to load in the browser
+    files: [
+      '../test/nuve-test.js',
+      '../extras/basic_example/public/erizo.js'
+    ],
+
+
+    // list of files to exclude
+    exclude: [
+      
+    ],
+
+
+    // test results reporter to use
+    // possible values: 'dots', 'progress', 'junit', 'growl', 'coverage'
+    reporters: ['progress'],
+
+
+    // web server port
+    port: 9876,
+
+
+    // enable / disable colors in the output (reporters and logs)
+    colors: true,
+
+
+    // level of logging
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    logLevel: config.LOG_INFO,
+
+
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: false,
+
+
+    // Start these browsers, currently available:
+    // - Chrome
+    // - ChromeCanary
+    // - Firefox
+    // - Opera
+    // - Safari (only Mac)
+    // - PhantomJS
+    // - IE (only Windows)
+    browsers: ['Chrome_with_fake_media'],
+
+    customLaunchers: {
+      Chrome_with_fake_media: {
+        base: 'Chrome',
+        flags: ['--disable-web-security --use-fake-ui-for-media-stream']
+      }
+    },
+
+    // If browser does not capture in given timeout [ms], kill it
+    captureTimeout: 60000,
+
+
+    // Continuous Integration mode
+    // if true, it capture browsers, run tests and exit
+    singleRun: true
+  });
+};
