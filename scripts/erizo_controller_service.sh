103f81e2cc14bbc250f3abf30d02a3d8dc6ed410
Jonathan R. Warden <jwarden@minervaproject.com>
2013-11-08 11:09:58 -0700

    added scripts to be called by supervisor

---
+++ scripts/erizo_controller_service.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+SCRIPT=`pwd`/$0
+FILENAME=`basename $SCRIPT`
+ROOT=`dirname $SCRIPT`
+CURRENT_DIR=`pwd`
+
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$ROOT/../erizo/build/erizo:$ROOT/../erizo:$ROOT/../build/libdeps/build/lib
+export ERIZO_HOME=$ROOT/erizo/
+
+cd $ROOT/erizoController
+node erizoController.js
---
+++ scripts/nuve_service.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+SCRIPT=`pwd`/$0
+ROOT=`dirname $SCRIPT`
+CURRENT_DIR=`pwd`
+
+cd $ROOT/nuveAPI
+
+node nuve.js
