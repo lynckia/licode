ad3b958c7f770917e02702bad0427a4393e0bdac
Jonathan R. Warden <jwarden@minervaproject.com>
2013-11-08 15:43:48 -0700

    make sure nuve starts before erizo.  Copy nuve.js ane erizo.js in installBasicExample.sh

--- scripts/erizo_controller_service.sh
+++ scripts/erizo_controller_service.sh
@@ -8,5 +8,8 @@ CURRENT_DIR=`pwd`
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$ROOT/../erizo/build/erizo:$ROOT/../erizo:$ROOT/../build/libdeps/build/lib
 export ERIZO_HOME=$ROOT/erizo/
 
+# Make sure nuve service has started
+sleep 2
+
 cd $ROOT/erizoController
 node erizoController.js
--- scripts/installBasicExample.sh
+++ scripts/installBasicExample.sh
@@ -13,4 +13,7 @@ cd $EXTRAS/basic_example
 
 npm install --loglevel debug express
 
+cp -r $ROOT/erizo_controller/erizoClient/dist/erizo.js $EXTRAS/basic_example/public/
+cp -r $ROOT/nuve/nuveClient/dist/nuve.js $EXTRAS/basic_example/
+
 cd $CURRENT_DIR
