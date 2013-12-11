2c988b73523dda5f07cf8f394baad8b416d4d0f7
Jonathan R. Warden <jwarden@minervaproject.com>
2013-12-11 12:59:01 -0800

    Fixed ERIZO_HOME settting in erizo_controller_service.sh

--- scripts/erizo_controller_service.sh
+++ scripts/erizo_controller_service.sh
@@ -2,14 +2,14 @@
 
 SCRIPT=`pwd`/$0
 FILENAME=`basename $SCRIPT`
-ROOT=`dirname $SCRIPT`/../erizo_controller
+ROOT=`dirname $SCRIPT`/..
 CURRENT_DIR=`pwd`
 
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$ROOT/../erizo/build/erizo:$ROOT/../erizo:$ROOT/../build/libdeps/build/lib
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$ROOT/erizo/build/erizo:$ROOT/erizo:$ROOT/build/libdeps/build/lib
 export ERIZO_HOME=$ROOT/erizo/
 
 # Make sure nuve service has started
 sleep 2
 
-cd $ROOT/erizoController
+cd $ROOT/erizo_controller/erizoController
 node erizoController.js
