d9f77054710bb3d8f9e9b600dd5724031dc39601
Javier Cerviño <jcervino@dit.upm.es>
2014-01-20 10:15:56 +0100

    Added patch to libnice 0.1.4 according to Yannick comments in mailing list to bypass Symmetric NATs

--- scripts/installUbuntuDeps.sh
+++ scripts/installUbuntuDeps.sh
@@ -56,6 +56,7 @@ install_libnice(){
     curl -O http://nice.freedesktop.org/releases/libnice-0.1.4.tar.gz
     tar -zxvf libnice-0.1.4.tar.gz
     cd libnice-0.1.4
+    patch -R ./agent/conncheck.c < $PATHNAME/libnice-014.patch0
     ./configure --prefix=$PREFIX_DIR
     make -s V=0
     make install
