7a185081fd66c6f357b83c6a381bc8dd4a213e59
Javier Cerviño <jcervino@dit.upm.es>
2014-05-08 10:31:43 +0200

    Added support to new resizer

--- erizo_controller/erizoClient/src/views/VideoPlayer.js
+++ erizo_controller/erizoClient/src/views/VideoPlayer.js
@@ -37,55 +37,11 @@ Erizo.VideoPlayer = function (spec) {
     // It will stop the VideoPlayer and remove it from the HTML
     that.destroy = function () {
         that.video.pause();
-        clearInterval(that.resize);
+        delete that.resizer;
         that.parentNode.removeChild(that.div);
     };
 
-    /*window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function (m, key, value) {
-        document.getElementById(key).value = unescape(value);
-    });*/
-
-    L.Logger.debug('Creating URL from stream ' + that.stream);
-    var myURL = window.URL || webkitURL;
-    that.stream_url = myURL.createObjectURL(that.stream);
-
-    // Container
-    that.div = document.createElement('div');
-    that.div.setAttribute('id', 'player_' + that.id);
-    that.div.setAttribute('style', 'width: 100%; height: 100%; position: relative; background-color: black; overflow: hidden;');
-
-    // Loader icon
-    that.loader = document.createElement('img');
-    that.loader.setAttribute('style', 'width: 16px; height: 16px; position: absolute; top: 50%; left: 50%; margin-top: -8px; margin-left: -8px');
-    that.loader.setAttribute('id', 'back_' + that.id);
-    that.loader.setAttribute('src', that.url + '/assets/loader.gif');
-
-    // Video tag
-    that.video = document.createElement('video');
-    that.video.setAttribute('id', 'stream' + that.id);
-    that.video.setAttribute('style', 'width: 100%; height: 100%; position: absolute');
-    that.video.setAttribute('autoplay', 'autoplay');
-
-    if(spec.stream.local) 
-        that.video.volume = 0;
-
-    if (that.elementID !== undefined) {
-        document.getElementById(that.elementID).appendChild(that.div);
-        that.container = document.getElementById(that.elementID);
-    } else {
-        document.body.appendChild(that.div);
-        that.container = document.body;
-    }
-
-    that.parentNode = that.div.parentNode;
-
-    that.div.appendChild(that.loader);
-    that.div.appendChild(that.video);
-
-    that.containerWidth = 0;
-    that.containerHeight = 0;
-
-    that.resize = setInterval(function () {
+    that.resize = function () {
 
         var width = that.container.offsetWidth,
             height = that.container.offsetHeight;
@@ -135,8 +91,55 @@ Erizo.VideoPlayer = function (spec) {
         that.containerWidth = width;
         that.containerHeight = height;
 
-    }, 500);
+    };
 
+    /*window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function (m, key, value) {
+        document.getElementById(key).value = unescape(value);
+    });*/
+
+    L.Logger.debug('Creating URL from stream ' + that.stream);
+    var myURL = window.URL || webkitURL;
+    that.stream_url = myURL.createObjectURL(that.stream);
+
+    // Container
+    that.div = document.createElement('div');
+    that.div.setAttribute('id', 'player_' + that.id);
+    that.div.setAttribute('style', 'width: 100%; height: 100%; position: relative; background-color: black; overflow: hidden;');
+
+    // Loader icon
+    that.loader = document.createElement('img');
+    that.loader.setAttribute('style', 'width: 16px; height: 16px; position: absolute; top: 50%; left: 50%; margin-top: -8px; margin-left: -8px');
+    that.loader.setAttribute('id', 'back_' + that.id);
+    that.loader.setAttribute('src', that.url + '/assets/loader.gif');
+
+    // Video tag
+    that.video = document.createElement('video');
+    that.video.setAttribute('id', 'stream' + that.id);
+    that.video.setAttribute('style', 'width: 100%; height: 100%; position: absolute');
+    that.video.setAttribute('autoplay', 'autoplay');
+
+    if(spec.stream.local) 
+        that.video.volume = 0;
+
+    if (that.elementID !== undefined) {
+        document.getElementById(that.elementID).appendChild(that.div);
+        that.container = document.getElementById(that.elementID);
+    } else {
+        document.body.appendChild(that.div);
+        that.container = document.body;
+    }
+
+    that.parentNode = that.div.parentNode;
+
+    that.div.appendChild(that.loader);
+    that.div.appendChild(that.video);
+
+    that.containerWidth = 0;
+    that.containerHeight = 0;
+
+    that.resizer = new L.ResizeSensor(that.container, that.resize);
+
+    that.resize();
 
     // Bottom Bar
     that.bar = new Erizo.Bar({elementID: 'player_' + that.id, id: that.id, stream: spec.stream, media: that.video, options: spec.options});
