f3778a419799e491306e9594f01ce21dc6b6d33e
Jeremy Noring <jnoring@hirevue.com>
2014-05-29 10:45:31 -0600

    Revert "Enable opus in the SDP exchange."
    This reverts commit 1d6019b3069b7551bee156ca8703ec13aaebae2e.

--- erizo/src/erizo/SdpInfo.cpp
+++ erizo/src/erizo/SdpInfo.cpp
@@ -63,7 +63,7 @@ namespace erizo {
     ulpfec.channels = 1;
     ulpfec.mediaType = VIDEO_TYPE;
     internalPayloadVector_.push_back(ulpfec);
-
+/*
     RtpMap opus;
     opus.payloadType = OPUS_48000_PT;
     opus.encodingName = "opus";
@@ -71,7 +71,7 @@ namespace erizo {
     opus.channels = 2;
     opus.mediaType = AUDIO_TYPE;
     internalPayloadVector_.push_back(opus);
-/*
+
     RtpMap isac16;
     isac16.payloadType = ISAC_16000_PT;
     isac16.encodingName = "ISAC";
