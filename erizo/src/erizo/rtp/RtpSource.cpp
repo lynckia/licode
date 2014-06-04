16d605e6a4101fe2c689c4a2f42f4fd70d4dbbda
lodoyun <lodoyun@gmail.com>
2014-06-04 10:05:10 +0200

    Added RtpSource moved RtpSink and removed it from OTMTranscoder and VideoMixer as it is not used

---
+++ erizo/src/erizo/rtp/RtpSource.cpp
@@ -0,0 +1,37 @@
+
+/*
+ * RtpSource.cpp
+ *
+ *  Created on: Aug 2, 2012
+ *      Author: pedro
+ */
+
+#include "RtpSource.h"
+using boost::asio::ip::udp;
+
+namespace erizo {
+
+RtpSource::RtpSource(const int port) {
+  socket_.reset(new boost::asio::ip::udp::socket(io_service_, boost::asio::ip::udp::endpoint(boost::asio::ip::udp::v4(), port)));
+  running_=true;
+  rtpSource_thread_= boost::thread(&RtpSource::getDataLoop,this);
+}
+
+RtpSource::~RtpSource() {
+  running_ = false;
+  rtpSource_thread_.join();
+
+}
+
+void RtpSource::getDataLoop() {
+  size_t length;
+  while(running_){
+    boost::asio::ip::udp::endpoint sender_endpoint;    
+    length = socket_->receive_from(boost::asio::buffer(buffer_, LENGTH), sender_endpoint);
+    if (length>0&&this->videoSink_){
+      this->videoSink_->deliverVideoData((char*)buffer_, (int)length);
+    }
+  }
+}
+
+} /* namespace erizo */
