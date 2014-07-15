4ead51fc0b41810d255243160adeea2bc91b9265
Gene Hallman <gene@minervaproject.com>
2014-07-15 15:57:52 -0700

    reenable opus and isac

--- erizo/src/erizo/SdpInfo.cpp
+++ erizo/src/erizo/SdpInfo.cpp
@@ -65,7 +65,7 @@ namespace erizo {
     ulpfec.channels = 1;
     ulpfec.mediaType = VIDEO_TYPE;
     internalPayloadVector_.push_back(ulpfec);
-/*
+*/
     RtpMap opus;
     opus.payloadType = OPUS_48000_PT;
     opus.encodingName = "opus";
@@ -89,7 +89,7 @@ namespace erizo {
     isac32.channels = 1;
     isac32.mediaType = AUDIO_TYPE;
     internalPayloadVector_.push_back(isac32);
-
+/*
     RtpMap pcmu;
     pcmu.payloadType = PCMU_8000_PT;
     pcmu.encodingName = "PCMU";
