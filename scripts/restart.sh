7bb5c56d81395578b51130cc7c0e1dc002601390
Jonathan R. Warden <jwarden@minervaproject.com>
2014-08-29 08:52:07 -0600

    change name or erizo agent service yet again

--- scripts/restart.sh
+++ scripts/restart.sh
@@ -4,12 +4,12 @@ sudo pkill node
 
 sudo supervisorctl stop licode_basic_server
 sudo supervisorctl stop erizo
-sudo supervisorctl start agent_erizo
+sudo supervisorctl start erizo_agent
 sudo supervisorctl stop nuve
 
 sudo supervisorctl start nuve
 sleep 1
 sudo supervisorctl start erizo
-sudo supervisorctl start agent_erizo
+sudo supervisorctl start erizo_agent
 sleep 1
 sudo supervisorctl start licode_basic_server
