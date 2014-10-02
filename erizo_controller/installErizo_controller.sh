5a64bd551b2ca4ed39e354401b213eabb52c5a57
Byron Clark <byron@theclarkfamily.name>
2014-10-02 17:16:44 -0600

    Log segfaults in native modules.
    When native code crashes, we don't get any kind of stack trace to help
    us debug. At least dump a stack trace now.
    
    Note: this uses a specific git commit of node-segfault-handler because
    the released version does not work correctly with node 0.10.x.

--- erizo_controller/installErizo_controller.sh
+++ erizo_controller/installErizo_controller.sh
@@ -2,7 +2,7 @@
 
 echo [erizo_controller] Installing node_modules for erizo_controller
 
-npm install --loglevel error amqp socket.io@0.9.16 log4js node-getopt
+npm install --loglevel error amqp socket.io@0.9.16 log4js node-getopt ddopson/node-segfault-handler.git#84cd583
 
 echo [erizo_controller] Done, node_modules installed
 
