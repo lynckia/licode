b3ff534a6aec868075cd7cbb0ee7a73b2eb57172
lodoyun <lodoyun@gmail.com>
2015-04-09 12:36:55 +0200

    Licode Sdps now react accordingly to a=sendonly(recvonly, etc.) lines. Identifiers of a=mid lines in Bundle are also supported

--- erizo/src/erizo/SdpInfo.h
+++ erizo/src/erizo/SdpInfo.h
@@ -24,6 +24,12 @@ enum HostType {
 enum MediaType {
     VIDEO_TYPE, AUDIO_TYPE, OTHER
 };
+/** 
+ * Stream directions
+ */
+enum StreamDirection {
+  SENDRECV, SENDONLY, RECVONLY
+};
 /**
  * RTP Profile
  */
@@ -79,6 +85,18 @@ public:
     std::string password;
     MediaType mediaType;
 };
+
+/**
+ * A bundle tag
+ */
+class BundleTag {
+  public:
+    BundleTag(std::string theId, MediaType theType):id(theId),mediaType(theType){
+    };
+    std::string id;
+    MediaType mediaType;
+};
+
 /**
  * A PT to Codec map
  */
@@ -200,6 +218,8 @@ public:
     * Is there rtcp muxing
     */
     bool isRtcpMux;
+    
+    StreamDirection direction;
     /**
     * RTP Profile type
     */
@@ -224,6 +244,7 @@ public:
      * The negotiated payload list
      */
     std::vector<RtpMap> payloadVector;
+    std::vector<BundleTag> bundleTags;
     /*
      * MLines for video and audio
      */
