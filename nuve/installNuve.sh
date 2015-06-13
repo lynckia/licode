8482e0014ad941b09e957e15437155474c0e8e0d
Pedro Rodriguez <lodoyun@gmail.com>
2015-06-13 12:43:05 +0200

    Set version number for mongojs
    1.0 version changes the API

--- nuve/installNuve.sh
+++ nuve/installNuve.sh
@@ -14,7 +14,7 @@ cd nuveAPI
 
 echo [nuve] Installing node_modules for nuve
 
-npm install --loglevel error amqp express mongojs aws-lib log4js node-getopt body-parser
+npm install --loglevel error amqp express mongojs@0.17 aws-lib log4js node-getopt body-parser
 echo [nuve] Done, node_modules installed
 
 cd ../nuveClient/tools
