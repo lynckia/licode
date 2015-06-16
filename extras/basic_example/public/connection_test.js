72e271321153b9970f4c67551bd1cfc462008971
aalonsog <aalonsog@dit.upm.es>
2015-06-16 12:39:41 +0200

    Added connectivity page to basic example

--- extras/basic_example/public/connection_test.js
+++ extras/basic_example/public/connection_test.js
@@ -1,5 +1,5 @@
 var serverUrl = "/";
-var localStream, room, recording, recordingId;
+var localStream, room;
 
 function getParameterByName(name) {
   name = name.replace(/[\[]/, "\\\[").replace(/[\]]/, "\\\]");
@@ -8,34 +8,12 @@ function getParameterByName(name) {
   return results == null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
 }
 
-function testConnection () {
-  window.location = "/connection_test.html";
-}
-
-function startRecording () {
-  if (room !== undefined){
-    if (!recording){
-      room.startRecording(localStream, function(id) {
-        recording = true;
-        recordingId = id;
-      });
-      
-    } else {
-      room.stopRecording(recordingId);
-      recording = false;
-    }
-  }
+function printText(text) {
+  document.getElementById('messages').value += '- ' + text + '\n';
 }
 
 window.onload = function () {
-  recording = false;
-  var screen = getParameterByName("screen");
-  var config = {audio: true, video: true, data: true, screen: screen, videoSize: [640, 480, 640, 480]};
-  // If we want screen sharing we have to put our Chrome extension id. The default one only works in our Lynckia test servers.
-  // If we are not using chrome, the creation of the stream will fail regardless.
-  if (screen){
-    config.extensionId = "okeephmleflklcdebijnponpabbmmgeo";
-  }
+  var config = {audio: true, video: true, data: true, videoSize: [640, 480, 640, 480]};
   localStream = Erizo.Stream(config);
   var createToken = function(userName, role, callback) {
 
@@ -60,37 +38,31 @@ window.onload = function () {
     room = Erizo.Room({token: token});
 
     localStream.addEventListener("access-accepted", function () {
+      printText('Mic and Cam OK');
       var subscribeToStreams = function (streams) {
         for (var index in streams) {
           var stream = streams[index];
-          if (localStream.getID() !== stream.getID()) {
-            room.subscribe(stream);
-          }
+          room.subscribe(stream);
         }
       };
 
       room.addEventListener("room-connected", function (roomEvent) {
-
+        printText('Connected to the room OK');
         room.publish(localStream, {maxVideoBW: 300});
-        subscribeToStreams(roomEvent.streams);
       });
 
       room.addEventListener("stream-subscribed", function(streamEvent) {
+        printText('Subscribed to your local stream OK');
         var stream = streamEvent.stream;
-        var div = document.createElement('div');
-        div.setAttribute("style", "width: 320px; height: 240px;");
-        div.setAttribute("id", "test" + stream.getID());
-
-        document.body.appendChild(div);
-        stream.show("test" + stream.getID());
+        stream.show("my_subscribed_video");
 
       });
 
       room.addEventListener("stream-added", function (streamEvent) {
+        printText('Local stream published OK');
         var streams = [];
         streams.push(streamEvent.stream);
         subscribeToStreams(streams);
-        document.getElementById("recordButton").disabled = false;
       });
 
       room.addEventListener("stream-removed", function (streamEvent) {
@@ -104,13 +76,13 @@ window.onload = function () {
       
       room.addEventListener("stream-failed", function (streamEvent){
           console.log("STREAM FAILED, DISCONNECTION");
+          printText('STREAM FAILED, DISCONNECTION');
           room.disconnect();
-
       });
 
       room.connect();
 
-      localStream.show("myVideo");
+      localStream.show("my_local_video");
 
     });
     localStream.init();
