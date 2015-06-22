a9b64b301d80579110d0f2006812cf1f9a017fd6
nachoaguadoc <nachoaguadoc@gmail.com>
2015-06-22 15:48:19 +0200

    Fixed bug when screensharing in Chrome

--- erizo_controller/erizoClient/src/Stream.js
+++ erizo_controller/erizoClient/src/Stream.js
@@ -80,8 +80,10 @@ Erizo.Stream = function (spec) {
         if ((spec.audio || spec.video || spec.screen) && spec.url === undefined) {
           L.Logger.debug("Requested access to local media");
           var videoOpt = spec.video;
-          if ((videoOpt == true|| spec.screen == true ) && that.videoSize !== undefined) {
+          if (videoOpt == true && that.videoSize !== undefined) {
             videoOpt = {mandatory: {minWidth: that.videoSize[0], minHeight: that.videoSize[1], maxWidth: that.videoSize[2], maxHeight: that.videoSize[3]}};
+          } else if (spec.screen == true){
+            videoOpt = true;
           }
           var opt = {video: videoOpt, audio: spec.audio, fake: spec.fake, screen: spec.screen, extensionId:that.extensionId};
           L.Logger.debug(opt);
