cc12509cddb64bc21a80e81b04a1b38e86bd8581
Patryk Majewski <patrykm@me.com>
2015-08-18 09:56:50 +0200

    Remove socket.io bundling in erizo.js client js
    Adding as external dep, let client decide how to load socket.io (requirejs, etc)

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -15,6 +15,7 @@ module.exports = function(config) {
     // list of files / patterns to load in the browser
     files: [
       'erizo-test.js',
+      '../erizo_controller/erizoClient/dist/socket.io.js',
       '../erizo_controller/erizoClient/dist/erizo.js',
       '../nuve/nuveClient/build/nuve.js',
       '../licode_config.js'
