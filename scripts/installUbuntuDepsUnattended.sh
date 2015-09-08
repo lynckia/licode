1e753f64df5e5d2602febc877d30e4a21bc68b97
aalonsog <aalonsog@dit.upm.es>
2015-09-08 10:54:21 +0200

    Unattended scripts fix

--- scripts/installUbuntuDepsUnattended.sh
+++ scripts/installUbuntuDepsUnattended.sh
@@ -44,12 +44,12 @@ check_proxy(){
 }
 
 install_apt_deps(){
-  sudo apt-get update
-  sudo apt-get install -qq python-software-properties
-  sudo apt-get install -qq software-properties-common
-  sudo add-apt-repository ppa:chris-lea/node.js
-  sudo apt-get update
-  sudo apt-get install -qq git make gcc g++ libssl-dev cmake libglib2.0-dev pkg-config nodejs libboost-regex-dev libboost-thread-dev libboost-system-dev liblog4cxx10-dev rabbitmq-server mongodb openjdk-6-jre curl libboost-test-dev
+  sudo apt-get update -y
+  sudo apt-get install -qq python-software-properties -y
+  sudo apt-get install -qq software-properties-common -y
+  sudo add-apt-repository ppa:chris-lea/node.js -y
+  sudo apt-get update -y
+  sudo apt-get install -qq git make gcc g++ libssl-dev cmake libglib2.0-dev pkg-config nodejs libboost-regex-dev libboost-thread-dev libboost-system-dev liblog4cxx10-dev rabbitmq-server mongodb openjdk-6-jre curl libboost-test-dev -y
   sudo npm install -g node-gyp
   sudo chown -R `whoami` ~/.npm ~/tmp/
 }
