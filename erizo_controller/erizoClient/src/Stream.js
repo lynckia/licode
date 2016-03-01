76b44afcf3292901ec96922af9a70ae51a59ce20
Pedro Rodriguez <lodoyun@gmail.com>
2016-03-01 12:22:05 +0100

    Check if stream is local or remote in config checking

--- erizo_controller/erizoClient/src/Stream.js
+++ erizo_controller/erizoClient/src/Stream.js
@@ -220,21 +220,26 @@ Erizo.Stream = function (spec) {
                 L.Logger.warning("Cannot update type of subscription");
                 config.video = undefined;
                 config.audio = undefined;
+                config.screen = undefined;
             }
         }else{  // on publish or subscribe
-            if (config.video === true && that.hasVideo() === false){
-                L.Logger.warning("Trying to subscribe to video when there is no video, won't subscribe to video");
-                config.video = false;
-            }
-            if (config.audio === true && that.hasAudio() === false){
-                L.Logger.warning("Trying to subscribe to audio when there is no audio, won't subscribe to audio");
-                config.audio = false;
+            if(that.local === false){ // check what we can subscribe to
+                if (config.video === true && that.hasVideo() === false){
+                    L.Logger.warning("Trying to subscribe to video when there is no video, won't subscribe to video");
+                    config.video = false;
+                }
+                if (config.audio === true && that.hasAudio() === false){
+                    L.Logger.warning("Trying to subscribe to audio when there is no audio, won't subscribe to audio");
+                    config.audio = false;
+                }
             }
         }
-        if (!that.hasVideo() && (config.slideShowMode === true)){
-            L.Logger.warning("Cannot enable slideShowMode if it is not a video subscription, please check your parameters");
-            config.slideShowMode = false;
-        }
+        if(that.local === false){
+            if (!that.hasVideo() && (config.slideShowMode === true)){
+                L.Logger.warning("Cannot enable slideShowMode if it is not a video subscription, please check your parameters");
+                config.slideShowMode = false;
+            }
+        } 
     };
 
     that.updateConfiguration = function (config, callback) {
