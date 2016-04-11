5ecb170bf637ae2fe3250544405bd2eaa392d9d2
Pedro Rodriguez <lodoyun@gmail.com>
2016-04-11 12:46:43 +0200

    Cleanup, removed experiments comments - moved to erizo_tree_v2

--- erizo_controller/erizoController/roomController.js
+++ erizo_controller/erizoController/roomController.js
@@ -79,98 +79,6 @@ exports.RoomController = function (spec) {
         
     };
 
-/*  Prototype method to add a branch to a erizo Tree
-    var shouldAddSurrogate = function(publisher_id, subscriber_id, options, callback){
-        //Create new erizoJS, assign it as surrogate of publisher_id (subscribe), return id
-        log.info("Adding a surrogate");
-        getErizoJS(function(id){
-            log.info("got a new Erizo with id", id);
-            
-//            log.info("Adding subscriber ", id, ' to publisher ', publisher_id);
-            var options = {};
-            if (options.audio === undefined) options.audio = true;
-            if (options.video === undefined) options.video = true;
-
-            
-            var argsAddPub = [publisher_id, 300, true];
-            log.info("Adding publisher to surrogate", "ErizoJS_"+id, "args",argsAddPub);
-            var statusBranch = false;
-            var statusRoot = false;
-            var branchQueue = [];
-            var rootQueue = [];
-            log.info("calling addPublisher on branch");
-            amqper.callRpc("ErizoJS_"+id, "addPublisher", argsAddPub, {callback: function(msg){
-                log.info("Message from branch", msg.type);
-                if (msg.type==='started'){
-                    statusbranch = 'started';
-                    log.info("branch Started");
-                    if (branchQueue.length){
-                        while (branchQueue.length>0){
-                            var themessage = branchQueue.shift();
-                            var argus = [publisher_id, undefined, themessage];
-                            console.log("Queued msg from Erizoroot, sending to Erizobranch", argus);
-                            amqper.callRpc("ErizoJS_"+id, "processSignaling", argus, {});
-                        }
-
-                    }
-
-                }else if (msg.type === 'offer' || msg.type ==='candidate'){
-                    var argsSignaling = [publisher_id, id, msg];
-                    console.log("msg from Erizobranch, should to Erizoroot?");
-                    if (statusroot){
-                        console.log("Yes, sending", argsSignaling);
-                        amqper.callRpc(getErizoQueue(publisher_id, id), "processSignaling", argsSignaling, {});
-                    }else{
-                        console.log("No, queuing", argsSignaling);
-                        rootQueue.push(msg);
-                    }
-                }else if (msg.type === 'ready'){
-                    log.info("Surrogate ready, firing after a second");
-                    setTimeout (callback(id), 1000);
-                }
-                
-            }});
-
-            log.info("calling addSubscriber on root");
-            var argsAddSub = [id, publisher_id, options];
-            amqper.callRpc(getErizoQueue(publisher_id), "addSubscriber", argsAddSub, {callback: function(mess){
-                log.info("Message from root", mess.type);
-                if (mess.type==='started'){
-                    statusroot = 'started';
-                    log.info("root Started");
-                    if (rootQueue.length){
-                        while (rootQueue.length>0){
-                            var themessage = rootQueue.shift();
-                            var argsSignaling = [publisher_id, id, themessage];
-                            console.log("Queued msg from Erizobranch, sending to Erizoroot", argsSignaling);
-                            amqper.callRpc(getErizoQueue(publisher_id, id), "processSignaling", argsSignaling, {});
-                        }
-
-                    }
-
-                }else if (mess.type === 'answer' || mess.type ==='candidate'){
-                    if (mess.type === 'answer'){
-                        mess.type = 'offer';
-                    }
-                    var argus = [publisher_id, subscriber_id, mess];
-
-                    console.log("signalling from Erizoroot, should send to branch?");
-                    if (statusbranch){
-                        console.log("Yes, sending", argus);
-                        amqper.callRpc("ErizoJS_"+id, "processSignaling", argus, {});
-                    }else{
-                        console.log("No, queuing", argus);
-                        branchQueue.push(mess);
-                    }
-                }
-            }});
-            subscribers[publisher_id].push({id:subscriber_id, surrogate_id:id});
-            // Track subscriber locally
-
-        });
-
-    }
-*/
     that.addEventListener = function(eventListener) {
         eventListeners.push(eventListener);
     };
@@ -186,7 +94,6 @@ exports.RoomController = function (spec) {
     	        var args = [publisher_id, url];
 
                 // Track publisher locally
-//                publishers[publisher_id] = {};
                 publishers[publisher_id] = erizo_id;
                 subscribers[publisher_id] = [];
     	        
@@ -211,9 +118,6 @@ exports.RoomController = function (spec) {
             // Track external outputs
             externalOutputs[url] = publisher_id;
 
-            // Track publisher locally
-            // publishers[publisher_id] = publisher_id;
-            // subscribers[publisher_id] = [];
             callback('success');
         } else {
             callback('error');
@@ -245,7 +149,6 @@ exports.RoomController = function (spec) {
 //            log.info("Sending signaling mess to erizoJS of st ", streamId, ' of peer ', peerId);
 
             var args = [streamId, peerId, msg];
-
             amqper.callRpc(getErizoQueue(streamId), "processSignaling", args, {});
 
         }
@@ -275,8 +178,6 @@ exports.RoomController = function (spec) {
                 }
             	log.debug("ErizoJS id:", erizo_id, "assigned for publisher", publisher_id);
                 // Track publisher locally
-
-                
                 // then we call its addPublisher method.
                 var args = [publisher_id, options];
                 publishers[publisher_id] = erizo_id;
@@ -324,49 +225,28 @@ exports.RoomController = function (spec) {
 
         if (publishers[publisher_id] !== undefined && subscribers[publisher_id].indexOf(subscriber_id) === -1) {
             log.info("Adding subscriber ", subscriber_id, ' to publisher ', publisher_id, "options", options);
-            
 
-//            if (true){
+            if (options.audio === undefined) options.audio = true;
+            if (options.video === undefined) options.video = true;
 
-                if (options.audio === undefined) options.audio = true;
-                if (options.video === undefined) options.video = true;
-
-                var args = [subscriber_id, publisher_id, options];
-                //TODO: Potential problem here if erizoJS is not reachable
-                amqper.callRpc(getErizoQueue(publisher_id, undefined), "addSubscriber", args, {callback: function (data){
-                    if (data === 'timeout'){
-                        if (retries < MAX_ERIZOJS_RETRIES){
-                            retries++;
-                            log.warn("ErizoJS timed out, trying again to subscribe to ", publisher_id, "from", subscriber_id, "number of tries", retries);
-                            that.addSubscriber(subscriber_id, publisher_id, options, callback, retries);
-                            return;
-                        }
-                        log.error("Can not contact to ErizoJS", publisher_id , " failed add Subscriber -- timeout");
-                        callback('timeout');
+            var args = [subscriber_id, publisher_id, options];
+            //TODO: Potential problem here if erizoJS is not reachable
+            amqper.callRpc(getErizoQueue(publisher_id, undefined), "addSubscriber", args, {callback: function (data){
+                if (data === 'timeout'){
+                    if (retries < MAX_ERIZOJS_RETRIES){
+                        retries++;
+                        log.warn("ErizoJS timed out, trying again to subscribe to ", publisher_id, "from", subscriber_id, "number of tries", retries);
+                        that.addSubscriber(subscriber_id, publisher_id, options, callback, retries);
                         return;
-                    }else if (data === 'initializing'){
-                        subscribers[publisher_id].push(subscriber_id);
                     }
-                    callback(data);
-                }});
-
-/*            }else{ // Prototype for erizo_trees
-                shouldAddSurrogate (publisher_id, subscriber_id, options, function(new_id){
-                    log.info("Surrogate ?", new_id);
-
-                    if (options.audio === undefined) options.audio = true;
-                    if (options.video === undefined) options.video = true;
-
-                    var args = [subscriber_id, publisher_id, options];
-
-                    subscribers[publisher_id].push({id:subscriber_id, surrogate:new_id});
-                    amqper.callRpc(getErizoQueue(publisher_id, subscriber_id), "addSubscriber", args, {callback: callback});
-                    // Track subscriber locally
-                });
-                
-
-            }
-            */
+                    log.error("Can not contact to ErizoJS", publisher_id , " failed add Subscriber -- timeout");
+                    callback('timeout');
+                    return;
+                }else if (data === 'initializing'){
+                    subscribers[publisher_id].push(subscriber_id);
+                }
+                callback(data);
+            }});
         }
     };
 
@@ -382,7 +262,6 @@ exports.RoomController = function (spec) {
             amqper.callRpc(getErizoQueue(publisher_id), "removePublisher", args, undefined);
 
             // Remove tracks
-            // TODO: Remove all surrogates
             var index = erizos[publishers[publisher_id]].publishers.indexOf(publisher_id);
             erizos[publishers[publisher_id]].publishers.splice(index, 1);
 
