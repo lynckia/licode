c226a4ed669704b705009f024e96a7df3cf5fe07
Pedro Rodriguez <lodoyun@gmail.com>
2016-04-21 12:45:14 +0200

    Finished renaming spine

--- spine/installSpine.sh
+++ spine/installSpine.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-echo [erizo_native_client] Installing node_modules for erizo_native_client
+echo [spine] Installing node_modules for Spine
 
 npm install --loglevel error socket.io-client@0.9.16 log4js node-getopt xmlhttprequest@1.5.0
 
-echo [erizo_native_client] Done, node_modules installed
+echo [spine] Done, node_modules installed
 
 cd ../erizo_controller/erizoClient/tools
 
 ./compilefc.sh
 
-cp ../dist/erizofc.js ../../../erizo_native_client/
+cp ../dist/erizofc.js ../../../spine/
 
-echo [erizo_native_client] Done, erizofc.js compiled
+echo [spine] Done, erizofc.js compiled
