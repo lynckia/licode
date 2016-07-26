e0d85a98547a2a61f53e4f4b69e271e98af1efe7
Francesco Durighetto <mail@francescodurighetto.com>
2016-07-26 17:12:01 +0200

    replaced aws-lib with aws-sdk

--- nuve/installNuve.sh
+++ nuve/installNuve.sh
@@ -14,7 +14,7 @@ cd nuveAPI
 
 echo [nuve] Installing node_modules for nuve
 
-npm install --loglevel error amqp express mongojs aws-lib log4js node-getopt body-parser
+npm install --loglevel error amqp express mongojs aws-sdk log4js node-getopt body-parser
 echo [nuve] Done, node_modules installed
 
 cd ../nuveClient/tools
