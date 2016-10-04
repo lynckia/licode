be9bc3cae62e3a0c787c7304f0acaf01026a6c22
Javier Cerviño <javierc@tokbox.com>
2016-10-04 12:22:07 +0200

    Update more deps

--- scripts/installTravisDeps.sh
+++ scripts/installTravisDeps.sh
@@ -50,9 +50,11 @@ install_openssl(){
 install_libnice(){
   if [ -d $LIB_DIR ]; then
     cd $LIB_DIR
-    curl -O http://nice.freedesktop.org/releases/libnice-0.1.4.tar.gz
-    tar -zxvf libnice-0.1.4.tar.gz > /dev/null 2> /dev/null
+    curl -O https://nice.freedesktop.org/releases/libnice-0.1.4.tar.gz
+    tar -zxvf libnice-0.1.4.tar.gz
     cd libnice-0.1.4
+    patch -R ./agent/conncheck.c < $PATHNAME/libnice-014.patch0 && patch -p1 < $PATHNAME/libnice-014.patch1
+    check_result $?
     ./configure --prefix=$PREFIX_DIR && make -s V=0 && make install
     check_result $?
     cd $CURRENT_DIR
@@ -110,8 +112,7 @@ install_mediadeps_nogpl(){
 
 install_libsrtp(){
   cd $ROOT/third_party/srtp
-  CFLAGS="-fPIC" ./configure --prefix=$PREFIX_DIR && make -s V=0 && make uninstall && make install
-  check_result $?
+  CFLAGS="-fPIC" ./configure --prefix=$PREFIX_DIR && make -s V=0 && make install
   cd $CURRENT_DIR
 }
 
