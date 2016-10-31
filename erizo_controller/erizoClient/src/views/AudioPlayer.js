37c161d70ecbefdc904a9c222c121472b5edb61f
thehunmonkgroup <chad@apartmentlines.com>
2016-10-31 06:14:59 -0400

    Add option to suppress bottom bar in stream display. (#472)
    Allows bottom bar to not be rendered if {bar: false} is passed. In
    this case, a 'media' prop is set on the stream object, to provide a
    consistent interface to the underlying audio or video object.

--- erizo_controller/erizoClient/src/views/AudioPlayer.js
+++ erizo_controller/erizoClient/src/views/AudioPlayer.js
@@ -80,14 +80,20 @@ Erizo.AudioPlayer = function (spec) {
         that.div.appendChild(that.audio);
 
         // Bottom Bar
-        that.bar = new Erizo.Bar({elementID: 'player_' + that.id,
-                                  id: that.id,
-                                  stream: spec.stream,
-                                  media: that.audio,
-                                  options: spec.options});
+        if (spec.options.bar !== false) {
+            that.bar = new Erizo.Bar({elementID: 'player_' + that.id,
+                                      id: that.id,
+                                      stream: spec.stream,
+                                      media: that.audio,
+                                      options: spec.options});
 
-        that.div.onmouseover = onmouseover;
-        that.div.onmouseout = onmouseout;
+            that.div.onmouseover = onmouseover;
+            that.div.onmouseout = onmouseout;
+        }
+        else {
+            // Expose a consistent object to manipulate the media.
+            that.media = that.audio;
+        }
 
     } else {
         // It will stop the AudioPlayer and remove it from the HTML
