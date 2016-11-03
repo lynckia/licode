4c5c5bb0882300938e0d42f6ee9605d87ad15792
Pedro Rodriguez <lodoyun@gmail.com>
2016-11-03 19:07:49 +0100

    Use gcc/g++ 5 in ubuntu

--- scripts/installTravisDeps.sh
+++ scripts/installTravisDeps.sh
@@ -28,6 +28,10 @@ check_result() {
 }
 
 install_apt_deps(){
+  sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
+  sudo apt-get update -y
+  sudo apt-get install -qq gcc-5 g++-5
+  sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-5 60 --slave /usr/bin/g++ g++ /usr/bin/g++-5
   npm install -g node-gyp
   sudo chown -R `whoami` ~/.npm ~/tmp/
 }
