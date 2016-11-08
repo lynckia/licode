a9e6ac36cc44e19ffedd4fc79e83b4622e6810a1
Javier Cerviño <jcague@gmail.com>
2016-11-08 14:33:07 +0100

    Fix openssl url in Travis

--- scripts/installTravisDeps.sh
+++ scripts/installTravisDeps.sh
@@ -38,7 +38,7 @@ install_apt_deps(){
 install_openssl(){
   if [ -d $LIB_DIR ]; then
     cd $LIB_DIR
-    curl -O http://www.openssl.org/source/openssl-1.0.1l.tar.gz
+    curl -O https://www.openssl.org/source/openssl-1.0.1l.tar.gz
     tar -zxvf openssl-1.0.1l.tar.gz > /dev/null 2> /dev/null
     cd openssl-1.0.1l
     ./config --prefix=$PREFIX_DIR -fPIC && make -s V=0 && make install
