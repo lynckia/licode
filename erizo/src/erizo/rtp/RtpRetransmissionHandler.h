a9f5bdb3ed9c458dc93c7f61d7a62fbdc3ba9bc5
Javier Cerviño <jcague@gmail.com>
2016-11-10 17:58:54 +0100

    Format code a bit

--- erizo/src/erizo/rtp/RtpRetransmissionHandler.h
+++ erizo/src/erizo/rtp/RtpRetransmissionHandler.h
@@ -27,25 +27,31 @@ class RtpRetransmissionHandler : public Handler {
       char* movingBuf = packet->data;
       int rtcpLength = 0;
       int totalLength = 0;
+
       do {
         movingBuf += rtcpLength;
         chead = reinterpret_cast<RtcpHeader*>(movingBuf);
         rtcpLength = (ntohs(chead->length) + 1) * 4;
         totalLength += rtcpLength;
+
         if (chead->packettype == RTCP_RTP_Feedback_PT) {
           contains_nack = true;
           uint16_t initial_seq_num = chead->getNackPid();
           uint16_t plb = chead->getNackBlp();
+
           for (int i = -1; i <= kNackBlpSize; i++) {
             uint16_t seq_num = initial_seq_num + i + 1;
             bool packet_nacked = i == -1 || (plb >> i) & 0x0001;
+
             if (packet_nacked) {
               std::shared_ptr<dataPacket> recovered;
+
               if (connection_->getVideoSinkSSRC() == chead->getSourceSSRC()) {
                 recovered = video_[getIndexInBuffer(seq_num)];
               } else if (connection_->getAudioSinkSSRC() == chead->getSourceSSRC()) {
                 recovered = audio_[getIndexInBuffer(seq_num)];
               }
+
               if (recovered.get()) {
                 RtpHeader *recovered_head = reinterpret_cast<RtpHeader*> (recovered->data);
                 if (recovered_head->getSeqNumber() == seq_num) {
