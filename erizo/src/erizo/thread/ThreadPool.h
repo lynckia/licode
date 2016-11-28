81ed46dde0b7ca41b0a37f847be0c95e67c6e06a
Javier Cerviño <jcague@gmail.com>
2016-11-28 13:55:45 +0100

    Apply PR comments

--- erizo/src/erizo/thread/ThreadPool.h
+++ erizo/src/erizo/thread/ThreadPool.h
@@ -10,37 +10,12 @@ namespace erizo {
 
 class ThreadPool {
  public:
-  explicit ThreadPool(unsigned int num_workers) : workers_{} {
-    for (unsigned int index = 0; index < num_workers; index++) {
-      workers_.push_back(std::make_shared<Worker>());
-    }
-  }
+  explicit ThreadPool(unsigned int num_workers);
+  ~ThreadPool();
 
-  ~ThreadPool() {
-    close();
-  }
-
-  std::shared_ptr<Worker> getLessUsedWorker() {
-    std::shared_ptr<Worker> choosed_worker = workers_.front();
-    for (auto worker : workers_) {
-      if (choosed_worker.use_count() > worker.use_count()) {
-        choosed_worker = worker;
-      }
-    }
-    return choosed_worker;
-  }
-
-  void start() {
-    for (auto worker : workers_) {
-      worker->start();
-    }
-  }
-
-  void close() {
-    for (auto worker : workers_) {
-      worker->close();
-    }
-  }
+  std::shared_ptr<Worker> getLessUsedWorker();
+  void start();
+  void close();
 
  private:
   std::vector<std::shared_ptr<Worker>> workers_;
