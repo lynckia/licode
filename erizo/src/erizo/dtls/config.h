bae96ab96813dce88d175e926acde2c17352ec21
Javier Cerviño <jcague@gmail.com>
2016-12-07 13:43:28 +0100

    Drop files that are not used by DTLS

--- erizo/src/erizo/dtls/config.h
+++
@@ -1,9 +0,0 @@
-#ifndef ERIZO_SRC_ERIZO_DTLS_CONFIG_H_
-#define ERIZO_SRC_ERIZO_DTLS_CONFIG_H_
-
-typedef unsigned char  UInt8;
-typedef unsigned short UInt16;  // NOLINT(runtime/int)
-typedef unsigned int   UInt32;  // NOLINT(runtime/int)
-typedef unsigned long long UInt64;  // NOLINT(runtime/int)
-
-#endif  // ERIZO_SRC_ERIZO_DTLS_CONFIG_H_
