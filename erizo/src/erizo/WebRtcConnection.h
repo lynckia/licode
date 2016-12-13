d6eb1f89f3da349d057c02b8c3d8864f7e55a6bf
Javier Cerviño <jcague@gmail.com>
2016-12-13 17:14:04 +0100

    Updated and moved webrtc dependency (#653)

--- erizo/src/erizo/WebRtcConnection.h
+++ erizo/src/erizo/WebRtcConnection.h
@@ -14,7 +14,7 @@
 #include "./Stats.h"
 #include "pipeline/Pipeline.h"
 #include "thread/Worker.h"
-#include "rtp/webrtc/fec_receiver_impl.h"
+#include "webrtc/modules/rtp_rtcp/include/ulpfec_receiver.h"
 #include "rtp/RtcpProcessor.h"
 #include "rtp/RtpExtensionProcessor.h"
 #include "rtp/RtpSlideShowHandler.h"
@@ -148,9 +148,9 @@ class WebRtcConnection: public MediaSink, public MediaSource, public FeedbackSin
   void setMetadata(std::map<std::string, std::string> metadata);
 
   // webrtc::RtpHeader overrides.
-  int32_t OnReceivedPayloadData(const uint8_t* payloadData, const uint16_t payloadSize,
+  int32_t OnReceivedPayloadData(const uint8_t* payloadData, size_t payloadSize,
                                 const webrtc::WebRtcRTPHeader* rtpHeader) override;
-  bool OnRecoveredPacket(const uint8_t* packet, int packet_length) override;
+  bool OnRecoveredPacket(const uint8_t* packet, size_t packet_length) override;
 
   void read(std::shared_ptr<dataPacket> packet);
   void write(std::shared_ptr<dataPacket> packet);
@@ -180,7 +180,7 @@ class WebRtcConnection: public MediaSink, public MediaSource, public FeedbackSin
   int stunPort_, minPort_, maxPort_;
   std::string stunServer_;
 
-  webrtc::FecReceiverImpl fec_receiver_;
+  std::unique_ptr<webrtc::UlpfecReceiver> fec_receiver_;
   boost::condition_variable cond_;
 
   time_point now_, mark_;
