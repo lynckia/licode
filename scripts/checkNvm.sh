b770a6a7a222e0e70c219826a84422264afa7b21
Pedro Rodriguez <lodoyun@gmail.com>
2016-12-16 12:15:50 +0100

    Fixed checkNVM

--- scripts/checkNvm.sh
+++ scripts/checkNvm.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 set -e 
+set +e
+command -v nvm | grep 'nvm' &> /dev/null
+if [ ! $? == 0 ]; then
 CHECK_SCRIPT=$(readlink -f "$0")
 CHECK_SCRIPT_PATH=$(dirname "$CHECK_SCRIPT")
 LINKED_DIR=$CHECK_SCRIPT_PATH/../build/libdeps/nvm
 export NVM_DIR=$(readlink -f "$LINKED_DIR")
 
-echo 'Checking nvm dir $NVM_DIR'
-set +e
-command -v nvm | grep 'string' &> /dev/null
-if [ ! $? == 0 ]; then
   if [ -s "$NVM_DIR/nvm.sh" ]; then
     . "$NVM_DIR/nvm.sh" # This loads nvm
   else
