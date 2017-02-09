fcbfe6ab1ba6fdfd6ef4bb97445691413dca9ccc
Pedro Rodriguez <lodoyun@gmail.com>
2017-02-09 15:55:54 +0100

    Added listeners + UTs

--- erizo/src/erizo/rtp/SenderBandwidthEstimantionHandler.cpp
+++ erizo/src/erizo/rtp/SenderBandwidthEstimantionHandler.cpp
@@ -6,13 +6,19 @@ namespace erizo {
 DEFINE_LOGGER(SenderBandwidthEstimationHandler, "rtp.SenderBandwidthEstimationHandler");
 
 SenderBandwidthEstimationHandler::SenderBandwidthEstimationHandler() :
-  connection_{nullptr}, initialized_{false}, enabled_{true}, packets_sent_{0},
+  connection_{nullptr}, bwe_listener_{nullptr}, initialized_{false}, enabled_{true},
+  period_packets_sent_{0}, estimated_bitrate_{0}, estimated_loss_{0}, estimated_rtt_{0},
   sender_bwe_{new SendSideBandwidthEstimation()} {};
 
 SenderBandwidthEstimationHandler::SenderBandwidthEstimationHandler(const SenderBandwidthEstimationHandler&& handler) :  // NOLINT
     connection_{handler.connection_},
+    bwe_listener_{handler.bwe_listener_},
     initialized_{handler.initialized_},
     enabled_{handler.enabled_},
+    period_packets_sent_{handler.period_packets_sent_},
+    estimated_bitrate_{handler.estimated_bitrate_},
+    estimated_loss_{handler.estimated_loss_},
+    estimated_rtt_{handler.estimated_rtt_},
     sender_bwe_{handler.sender_bwe_},
     sr_delay_data_{std::move(handler.sr_delay_data_)} {}
 
@@ -40,125 +46,109 @@ void SenderBandwidthEstimationHandler::notifyUpdate() {
 }
 
 void SenderBandwidthEstimationHandler::read(Context *ctx, std::shared_ptr<dataPacket> packet) {
-  // calculate rtt
-  // Pass REMBs
-  // pass RR info + delay + packets sent
   RtcpHeader *chead = reinterpret_cast<RtcpHeader*>(packet->data);
-  if (chead->isFeedback()) {
-    if (chead->getBlockCount() == 0 && (chead->getLength() + 1) * 4  == packet->length) {
-      ELOG_DEBUG("Ignoring empty RR");
-      ctx->fireRead(packet);
-      return;
-    }
-    if (chead->getSourceSSRC() != connection_->getVideoSinkSSRC()) {
-      ELOG_DEBUG("Ignoring no video Packet sourceSSRC %u ssrc %u",
-          chead->getSourceSSRC(), chead->getSSRC());
-      ctx->fireRead(packet);
-      return;
-    }
-    // We try to add it just in case it is not there yet (otherwise its noop)
-    char* movingBuf = packet->data;
-    int rtcpLength = 0;
-    int totalLength = 0;
-    int currentBlock = 0;
-    uint32_t calculateLastSr, calculatedlsr;
-    int64_t nowms;
-    uint32_t delay;
-    uint64_t sentts, bitrate;
-    int estimated_bitrate;
-    uint8_t estimated_loss;
-    int64_t estimated_rtt;
+  if (chead->isFeedback() && chead->getSourceSSRC() == connection_->getVideoSinkSSRC()) {
+    char* packet_pointer = packet->data;
+    int rtcp_length = 0;
+    int total_length = 0;
+    int current_block = 0;
 
     do {
-      movingBuf+=rtcpLength;
-      chead = reinterpret_cast<RtcpHeader*>(movingBuf);
-      rtcpLength = (ntohs(chead->length) + 1) * 4;
-      totalLength += rtcpLength;
+      packet_pointer+=rtcp_length;
+      chead = reinterpret_cast<RtcpHeader*>(packet_pointer);
+      rtcp_length = (ntohs(chead->length) + 1) * 4;
+      total_length += rtcp_length;
       ELOG_DEBUG("%s ssrc %u, sourceSSRC %u, PacketType %u", connection_->toLog(),
           chead->getSSRC(),
           chead->getSourceSSRC(),
           chead->getPacketType());
       switch (chead->packettype) {
         case RTCP_Receiver_PT:
-          ELOG_DEBUG("Analyzing Video RR: PacketLost %u, Ratio %u, currentBlock %d, blocks %d"
-              ", sourceSSRC %u, ssrc %u changed to %u",
-              chead->getLostPackets(),
-              chead->getFractionLost(),
-              currentBlock,
-              chead->getBlockCount(),
-              chead->getSourceSSRC(),
-              chead->getSSRC(),
-              connection_->getVideoSinkSSRC());
-          // calculate RTT + Update receiver block
-          calculateLastSr = chead->getLastSr();
-          calculatedlsr = (chead->getDelaySinceLastSr() * 1000) / 65536;
-          nowms = ClockUtils::timePointToMs(clock::now());
-          for (auto it = sr_delay_data_.begin();
-              it != sr_delay_data_.end(); ++it) {
-            if ((*it)->sr_ntp == calculateLastSr) {
-              sentts = (*it)->sr_send_time;
-              delay = nowms - sentts - calculatedlsr;
-              sender_bwe_->UpdateReceiverBlock(chead->getFractionLost(),
-                  delay, packets_sent_, nowms);
-              packets_sent_ = 0;
-              ELOG_DEBUG("Calculated delay %u", delay);
-              sender_bwe_->CurrentEstimate(&estimated_bitrate, &estimated_loss,
-                  &estimated_rtt);
-              ELOG_DEBUG("Estimated bitrate %u, loss %u, rtt %lld", estimated_bitrate,
-                  estimated_loss, estimated_rtt);
+          {
+            ELOG_DEBUG("%s, Analyzing Video RR: PacketLost %u, Ratio %u, current_block %d, blocks %d"
+                ", sourceSSRC %u, ssrc %u",
+                connection_->toLog(),
+                chead->getLostPackets(),
+                chead->getFractionLost(),
+                current_block,
+                chead->getBlockCount(),
+                chead->getSourceSSRC(),
+                chead->getSSRC());
+            // calculate RTT + Update receiver block
+            uint32_t delay_since_last_ms = (chead->getDelaySinceLastSr() * 1000) / 65536;
+            int64_t now_ms = ClockUtils::timePointToMs(clock::now());
+            uint32_t last_sr = chead->getLastSr();
+
+            auto value = std::find_if(sr_delay_data_.begin(), sr_delay_data_.end(),
+                [last_sr](const std::shared_ptr<SrDelayData> sr_info) {
+                return sr_info->sr_ntp == last_sr;
+                });
+            if (value != sr_delay_data_.end()) {
+                uint32_t delay = now_ms - (*value)->sr_send_time - delay_since_last_ms;
+                sender_bwe_->UpdateReceiverBlock(chead->getFractionLost(),
+                    delay, period_packets_sent_, now_ms);
+                period_packets_sent_ = 0;
+                updateEstimate();
             }
           }
           break;
         case RTCP_PS_Feedback_PT:
-          if (chead->getBlockCount() == RTCP_AFB) {
-            char *uniqueId = reinterpret_cast<char*>(&chead->report.rembPacket.uniqueid);
-            if (!strncmp(uniqueId, "REMB", 4)) {
-              nowms = ClockUtils::timePointToMs(clock::now());
-              bitrate = chead->getBrMantis() << chead->getBrExp();
-              ELOG_DEBUG("REMB %llu", bitrate);
-              sender_bwe_->UpdateReceiverEstimate(nowms, bitrate);
-              // update bwe
-            } else {
-              ELOG_WARN("Unsupported AFB Packet not REMB")
+          {
+            if (chead->getBlockCount() == RTCP_AFB) {
+              char *uniqueId = reinterpret_cast<char*>(&chead->report.rembPacket.uniqueid);
+              if (!strncmp(uniqueId, "REMB", 4)) {
+                int64_t now_ms = ClockUtils::timePointToMs(clock::now());
+                uint64_t bitrate = chead->getBrMantis() << chead->getBrExp();
+                ELOG_DEBUG("%s message: Updating Estimate with REMB, bitrate %llu", connection_->toLog(),
+                    bitrate);
+                sender_bwe_->UpdateReceiverEstimate(now_ms, bitrate);
+                sender_bwe_->UpdateEstimate(now_ms);
+                updateEstimate();
+              } else {
+                ELOG_DEBUG("%s message: Unsupported AFB Packet not REMB", connection_->toLog());
+              }
             }
           }
           break;
         default:
           break;
       }
-      currentBlock++;
-    } while (totalLength < packet->length);
+      current_block++;
+    } while (total_length < packet->length);
   }
   ctx->fireRead(packet);
 }
 
 void SenderBandwidthEstimationHandler::write(Context *ctx, std::shared_ptr<dataPacket> packet) {
   RtcpHeader *chead = reinterpret_cast<RtcpHeader*>(packet->data);
-  if (!chead->isRtcp()) {
-    packets_sent_++;
-  } else if (chead->getPacketType() == RTCP_Sender_PT) {
+  if (!chead->isRtcp() && packet->type == VIDEO_PACKET) {
+    period_packets_sent_++;
+  } else if (chead->getPacketType() == RTCP_Sender_PT &&
+      chead->getSSRC() == connection_->getVideoSinkSSRC()) {
     analyzeSr(chead);
   }
-  // account rtp packets sent
-  // Analyze SR for delay
   ctx->fireWrite(packet);
 }
 
 void SenderBandwidthEstimationHandler::analyzeSr(RtcpHeader* chead) {
-  // We try to add it just in case it is not there yet (otherwise its noop)
-  ELOG_DEBUG("%s analyzeSR", connection_->toLog());
-  if (chead->getSSRC() != connection_->getVideoSinkSSRC()) {
-    ELOG_DEBUG("%s ignoring SR, ssrc %u", connection_->toLog(), chead->getSSRC());
-  }
   uint64_t now = ClockUtils::timePointToMs(clock::now());
   uint32_t ntp;
   uint64_t total_ntp = chead->getNtpTimestamp();
-  ntp = (total_ntp & (0xFFFFFFFF0000)) >> 16;
+  ntp = chead->get32MiddleNtp();
+  ELOG_DEBUG("%s message: adding incoming SR to list, ntp: %u", connection_->toLog(), ntp);
   sr_delay_data_.push_back(std::shared_ptr<SrDelayData>( new SrDelayData(ntp, now)));
-  // We only store the last 20 sr
   if (sr_delay_data_.size() >= kMaxSrListSize) {
     sr_delay_data_.pop_front();
   }
 }
 
+void SenderBandwidthEstimationHandler::updateEstimate() {
+  sender_bwe_->CurrentEstimate(&estimated_bitrate_, &estimated_loss_,
+      &estimated_rtt_);
+  ELOG_DEBUG("%s message: estimated bitrate %d, loss %u, rtt %lld",
+      connection_->toLog(), estimated_bitrate_, estimated_loss_, estimated_rtt_);
+  if (bwe_listener_) {
+    bwe_listener_->onBandwidthEstimate(estimated_bitrate_, estimated_loss_, estimated_rtt_);
+  }
+}
 }  // namespace erizo
--- erizo/src/erizo/rtp/SenderBandwidthEstimationHandler.h
+++ erizo/src/erizo/rtp/SenderBandwidthEstimationHandler.h
@@ -10,6 +10,12 @@
 namespace erizo {
 using webrtc::SendSideBandwidthEstimation;
 
+class SenderBandwidthEstimationListener {
+ public:
+  virtual void onBandwidthEstimate(int estimated_bitrate, uint8_t estimated_loss,
+      int64_t estimated_rtt) = 0;
+};
+
 class SenderBandwidthEstimationHandler : public Handler,
   public std::enable_shared_from_this<SenderBandwidthEstimationHandler> {
   DECLARE_LOGGER();
@@ -32,14 +38,24 @@ class SenderBandwidthEstimationHandler : public Handler,
 
   void analyzeSr(RtcpHeader *head);
 
+  void setListener(SenderBandwidthEstimationListener* listener) {
+    bwe_listener_ = listener;
+  }
+
  private:
   static const uint16_t kMaxSrListSize = 20;
   WebRtcConnection* connection_;
+  SenderBandwidthEstimationListener* bwe_listener_;
   bool initialized_;
   bool enabled_;
-  uint32_t packets_sent_;
+  uint32_t period_packets_sent_;
+  int estimated_bitrate_;
+  uint8_t estimated_loss_;
+  int64_t estimated_rtt_;
   std::shared_ptr<SendSideBandwidthEstimation> sender_bwe_;
   std::list<std::shared_ptr<SrDelayData>> sr_delay_data_;
+
+  void updateEstimate();
 };
 }  // namespace erizo
 #endif  //  ERIZO_SRC_ERIZO_RTP_SENDERBANDWIDTHESTIMATIONHANDLER_H_
