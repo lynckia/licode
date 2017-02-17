58ad42f1b027ffc052f19cbb085102fbd75038c7
simone <simone.mazzoni13@gmail.com>
2017-02-17 10:50:48 +0100

    New stream id generation function that guarrantees that the id is a 18 digits safe integer.

--- erizo_controller/erizoController/erizoController.js
+++ erizo_controller/erizoController/erizoController.js
@@ -519,7 +519,9 @@ var listen = function () {
                         return callback(null, 'Unauthorized');
                 }
             }
-            id = Math.random() * 1000000000000000000;
+
+            // this guarantees that the generated id is a safe 18 digits integer
+            id = Math.floor(100000000000000000 + Math.random() * 900000000000000000);
 
             if (options.state === 'url' || options.state === 'recording') {
                 var url = sdp;
