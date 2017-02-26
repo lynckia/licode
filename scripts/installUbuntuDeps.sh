ef2a412498a719e8cc48237a54771b253355b963
fanchuanster@gmail.com <fanchuanster@gmail.com>
2017-02-27 07:33:04 +0800

    adding curl install back

--- scripts/installUbuntuDeps.sh
+++ scripts/installUbuntuDeps.sh
@@ -70,7 +70,7 @@ install_apt_deps(){
   sudo apt-get install  software-properties-common -y
   sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
   sudo apt-get update -y
-  sudo apt-get install git make gcc-5 g++-5 libssl-dev cmake libglib2.0-dev pkg-config libboost-regex-dev libboost-thread-dev libboost-system-dev liblog4cxx10-dev rabbitmq-server mongodb openjdk-8-jre  libboost-test-dev -y --no-install-recommends
+  sudo apt-get install git make gcc-5 g++-5 libssl-dev cmake libglib2.0-dev pkg-config libboost-regex-dev libboost-thread-dev libboost-system-dev liblog4cxx10-dev rabbitmq-server mongodb openjdk-8-jre curl libboost-test-dev -y --no-install-recommends
   sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-5 60 --slave /usr/bin/g++ g++ /usr/bin/g++-5
 
   sudo chown -R `whoami` ~/.npm ~/tmp/ || true
