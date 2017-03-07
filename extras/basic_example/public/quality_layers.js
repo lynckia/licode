c9e065101d36ff0f1fee26e17ac46db4b170ce91
Pedro Rodriguez <lodoyun@gmail.com>
2017-03-07 13:59:36 +0100

    Fixed quality_layers when shouldPublish is not active

--- extras/basic_example/public/quality_layers.js
+++ extras/basic_example/public/quality_layers.js
@@ -5,10 +5,10 @@ var localStream, room, chart;
 var streamToChart;
 
 function getParameterByName(name) {
-  name = name.replace(/[\[]/, '\\\[').replace(/[\]]/, '\\\]');
-  var regex = new RegExp('[\\?&]' + name + '=([^&#]*)'),
-      results = regex.exec(location.search);
-  return results == null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
+    name = name.replace(/[\[]/, '\\\[').replace(/[\]]/, '\\\]');
+    var regex = new RegExp('[\\?&]' + name + '=([^&#]*)'),
+        results = regex.exec(location.search);
+    return results == null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
 }
 
 var seriesMap = {};
@@ -19,7 +19,7 @@ var initChart = function () {
             renderTo: 'chart',
             defaultSeriesType: 'spline',
             events: {
-            //    load: updateChart
+                //    load: updateChart
             }
         },
         title: {
@@ -55,29 +55,29 @@ var updateSeriesForKey = function (key, value_x, value_y) {
 }
 
 var updateChart = function () {
-   // console.log("UpdateChart", chart);
+    // console.log("UpdateChart", chart);
     let date = (new Date()).getTime();
     room.getStreamStats(streamToChart, function(data) {
         for (var i in data) {
             if (i != "publisher") {
-               let totalBitrate = data[i]["total"]["bitrateCalculated"];
-               if (totalBitrate) {
-                   updateSeriesForKey("Current Received",date, totalBitrate);
-               }
-               let bitrateEstimated = data[i]["total"]["senderBitrateEstimation"]; 
-               if (bitrateEstimated) {
-                   updateSeriesForKey("Estimated Bandwidth",date, totalBitrate);
-               }
-               let qualityLayersData = data[i]["qualityLayers"];
-               if (qualityLayersData !== undefined){
-                   for (var spatialLayer in qualityLayersData) {
-                       for (var temporalLayer in qualityLayersData[spatialLayer]) {
-                           let key = "Spatial " + spatialLayer + " / Temporal " + temporalLayer;
-                           updateSeriesForKey(key, date, qualityLayersData[spatialLayer][temporalLayer])
-                       }
-                   }
+                let totalBitrate = data[i]["total"]["bitrateCalculated"];
+                if (totalBitrate) {
+                    updateSeriesForKey("Current Received",date, totalBitrate);
+                }
+                let bitrateEstimated = data[i]["total"]["senderBitrateEstimation"]; 
+                if (bitrateEstimated) {
+                    updateSeriesForKey("Estimated Bandwidth",date, totalBitrate);
+                }
+                let qualityLayersData = data[i]["qualityLayers"];
+                if (qualityLayersData !== undefined){
+                    for (var spatialLayer in qualityLayersData) {
+                        for (var temporalLayer in qualityLayersData[spatialLayer]) {
+                            let key = "Spatial " + spatialLayer + " / Temporal " + temporalLayer;
+                            updateSeriesForKey(key, date, qualityLayersData[spatialLayer][temporalLayer])
+                        }
+                    }
 
-               }
+                }
             }
         }
         setTimeout(updateChart, 1000);    
@@ -86,114 +86,109 @@ var updateChart = function () {
 }
 
 window.onload = function () {
-  var screen = getParameterByName('screen');
-  var roomName = getParameterByName('room') || 'basicExampleRoom';
-  console.log('Selected Room', room);
-  var config = {audio: true,
-                video: true,
-                data: true,
-                screen: screen,
-                videoSize: [640, 480, 1280, 720],
-                videoFrameRate: [10, 20]};
-  // If we want screen sharing we have to put our Chrome extension id.
-  // The default one only works in our Lynckia test servers.
-  // If we are not using chrome, the creation of the stream will fail regardless.
-  if (screen){
-    config.extensionId = 'okeephmleflklcdebijnponpabbmmgeo';
-  }
-  localStream = Erizo.Stream(config);
-  var createToken = function(userName, role, roomName, callback) {
+    var screen = getParameterByName('screen');
+    var roomName = getParameterByName('room') || 'basicExampleRoom';
+    console.log('Selected Room', room);
+    var config = {audio: true,
+        video: true,
+        data: true,
+        screen: screen,
+        videoSize: [640, 480, 1280, 720],
+        videoFrameRate: [10, 20]};
+    // If we want screen sharing we have to put our Chrome extension id.
+    // The default one only works in our Lynckia test servers.
+    // If we are not using chrome, the creation of the stream will fail regardless.
+    if (screen){
+        config.extensionId = 'okeephmleflklcdebijnponpabbmmgeo';
+    }
+    localStream = Erizo.Stream(config);
+    var createToken = function(userName, role, roomName, callback) {
 
-    var req = new XMLHttpRequest();
-    var url = serverUrl + 'createToken/';
-    var body = {username: userName, role: role, room:roomName};
+        var req = new XMLHttpRequest();
+        var url = serverUrl + 'createToken/';
+        var body = {username: userName, role: role, room:roomName};
 
-    req.onreadystatechange = function () {
-      if (req.readyState === 4) {
-        callback(req.responseText);
-      }
+        req.onreadystatechange = function () {
+            if (req.readyState === 4) {
+                callback(req.responseText);
+            }
+        };
+
+        req.open('POST', url, true);
+        req.setRequestHeader('Content-Type', 'application/json');
+        req.send(JSON.stringify(body));
     };
 
-    req.open('POST', url, true);
-    req.setRequestHeader('Content-Type', 'application/json');
-    req.send(JSON.stringify(body));
-  };
+    createToken('user', 'presenter', roomName, function (response) {
+        var token = response;
+        console.log(token);
+        room = Erizo.Room({token: token});
 
-  createToken('user', 'presenter', roomName, function (response) {
-    var token = response;
-    console.log(token);
-    room = Erizo.Room({token: token});
+        localStream.addEventListener('access-accepted', function () {
+            var subscribeToStreams = function (streams) {
+                for (var index in streams) {
+                    var stream = streams[index];
+                    room.subscribe(stream, {slideShowMode: false, metadata: {type: 'subscriber'}});
+                    return;
+                }
+            };
 
-    localStream.addEventListener('access-accepted', function () {
-      var subscribeToStreams = function (streams) {
-        var cb = function (evt){
-            console.log('Bandwidth Alert', evt.msg, evt.bandwidth);
-        };
-        for (var index in streams) {
-          var stream = streams[index];
-          if (localStream.getID() === stream.getID()) {
-            room.subscribe(stream, {slideShowMode: false, metadata: {type: 'subscriber'}});
-          }
-        }
-      };
+            room.addEventListener('room-connected', function (roomEvent) {
+                console.log("InitChart");
+                initChart();
+                console.log("Chart inited", chart);
+                var options = {metadata: {type: 'publisher'}};
+                var enableSimulcast = true; 
+                if (enableSimulcast) options._simulcast = {numSpatialLayers: 2};
 
-      room.addEventListener('room-connected', function (roomEvent) {
-        console.log("InitChart");
-        initChart();
-        console.log("Chart inited", chart);
-        var options = {metadata: {type: 'publisher'}};
-        var enableSimulcast = true; 
-        if (enableSimulcast) options._simulcast = {numSpatialLayers: 2};
+                var shouldPublish = getParameterByName('shouldPublish');
+                if (shouldPublish) {
+                    room.publish(localStream, options);
+                } else {
+                    subscribeToStreams(roomEvent.streams);
+                }
+            });
 
-        var shouldPublish = getParameterByName('shouldPublish');
-        if (shouldPublish) {
-            room.publish(localStream, options);
-        }
-        subscribeToStreams(roomEvent.streams);
-      });
+            room.addEventListener('stream-subscribed', function(streamEvent) {
+                var stream = streamEvent.stream;
+                var div = document.createElement('div');
+                div.setAttribute('style', 'width: 640px; height: 480px;');
+                div.setAttribute('id', 'test' + stream.getID());
 
-      room.addEventListener('stream-subscribed', function(streamEvent) {
-        var stream = streamEvent.stream;
-        var div = document.createElement('div');
-        div.setAttribute('style', 'width: 640px; height: 480px;');
-        div.setAttribute('id', 'test' + stream.getID());
-        
-          if (streamToChart == undefined) {
-            console.log("saving stream", stream);
-            streamToChart = stream;
-            updateChart();
-        }
+                if (streamToChart == undefined) {
+                    console.log("Saving stream", stream);
+                    streamToChart = stream;
+                    updateChart();
+                }
 
-        document.body.appendChild(div);
-        stream.show('test' + stream.getID());
+                document.body.appendChild(div);
+                stream.show('test' + stream.getID());
 
-      });
+            });
 
-      room.addEventListener('stream-added', function (streamEvent) {
-        var streams = [];
-        streams.push(streamEvent.stream);
-        subscribeToStreams(streams);
-      });
+            room.addEventListener('stream-added', function (streamEvent) {
+                var streams = [];
+                streams.push(streamEvent.stream);
+                subscribeToStreams(streams);
+            });
 
-      room.addEventListener('stream-removed', function (streamEvent) {
-        // Remove stream from DOM
-        var stream = streamEvent.stream;
-        if (stream.elementID !== undefined) {
-          var element = document.getElementById(stream.elementID);
-          document.body.removeChild(element);
-        }
-      });
+            room.addEventListener('stream-removed', function (streamEvent) {
+                // Remove stream from DOM
+                var stream = streamEvent.stream;
+                if (stream.elementID !== undefined) {
+                    var element = document.getElementById(stream.elementID);
+                    document.body.removeChild(element);
+                }
+            });
 
-      room.addEventListener('stream-failed', function (){
-          console.log('Stream Failed, act accordingly');
-      });
+            room.addEventListener('stream-failed', function (){
+                console.log('Stream Failed, act accordingly');
+            });
 
-      room.connect();
+            room.connect();
 
-//      localStream.show('myVideo');
+        });
 
+        localStream.init();
     });
-      
-    localStream.init();
-  });
 };
