9bf586d5bf7d47ed193caeee5de7fe81f31b9bb3
Pedro Rodriguez <lodoyun@gmail.com>
2017-04-11 16:31:32 +0200

    Added translator map to support multiple ssrcs

--- erizo/src/erizo/rtp/RtpPaddingRemovalHandler.cpp
+++ erizo/src/erizo/rtp/RtpPaddingRemovalHandler.cpp
@@ -18,14 +18,24 @@ void RtpPaddingRemovalHandler::disable() {
 
 void RtpPaddingRemovalHandler::read(Context *ctx, std::shared_ptr<dataPacket> packet) {
   RtcpHeader *chead = reinterpret_cast<RtcpHeader*>(packet->data);
+  RtpHeader *rtp_header = reinterpret_cast<RtpHeader*>(packet->data);
 
   if (!chead->isRtcp() && enabled_ && packet->type == VIDEO_PACKET) {
-    if (!removePaddingBytes(packet)) {
+    uint32_t ssrc = rtp_header->getSSRC();
+    auto translator_it = translator_map_.find(ssrc);
+    std::shared_ptr<SequenceNumberTranslator> translator;
+    if (translator_it != translator_map_.end()) {
+      translator = translator_it->second;
+    } else {
+      ELOG_DEBUG("message: no Translator found creating a new one, ssrc: %u", ssrc);
+      translator = std::make_shared<SequenceNumberTranslator>();
+      translator_map_[ssrc] = translator;
+    }
+    if (!removePaddingBytes(packet, translator)) {
       return;
     }
-    RtpHeader *rtp_header = reinterpret_cast<RtpHeader*>(packet->data);
     uint16_t sequence_number = rtp_header->getSeqNumber();
-    SequenceNumber sequence_number_info = translator_.get(sequence_number, false);
+    SequenceNumber sequence_number_info = translator->get(sequence_number, false);
 
     if (sequence_number_info.type != SequenceNumberType::Valid) {
       return;
@@ -39,14 +49,15 @@ void RtpPaddingRemovalHandler::write(Context *ctx, std::shared_ptr<dataPacket> p
   ctx->fireWrite(packet);
 }
 
-bool RtpPaddingRemovalHandler::removePaddingBytes(std::shared_ptr<dataPacket> packet) {
+bool RtpPaddingRemovalHandler::removePaddingBytes(std::shared_ptr<dataPacket> packet,
+    std::shared_ptr<SequenceNumberTranslator> translator) {
   RtpHeader *rtp_header = reinterpret_cast<RtpHeader*>(packet->data);
   int header_length = rtp_header->getHeaderLength();
 
   int padding_length = RtpUtils::getPaddingLength(packet);
   if (padding_length + header_length == packet->length) {
     uint16_t sequence_number = rtp_header->getSeqNumber();
-    translator_.get(sequence_number, true);
+    translator->get(sequence_number, true);
     return false;
   }
   packet->length -= padding_length;
