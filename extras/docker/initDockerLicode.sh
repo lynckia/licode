0a867d5d1e2a3a1079c7adc01d09b9cec80c45a8
Pedro Rodriguez <lodoyun@gmail.com>
2017-04-25 14:46:32 +0200

    Fixed docker licode start script

--- extras/docker/initDockerLicode.sh
+++ extras/docker/initDockerLicode.sh
@@ -1,7 +1,20 @@
 #!/usr/bin/env bash
+SCRIPT=`pwd`/$0
+ROOT=/opt/licode
+EXTRAS=$ROOT/extras
+NVM_CHECK=$ROOT/scripts/checkNvm.sh
+
+
 echo "config.erizoController.publicIP = '$PUBLIC_IP';" >> /opt/licode/licode_config.js
 mongod --dbpath /opt/licode/build/db --logpath /opt/licode/build/mongo.log --fork
+rabbitmq-server -detached
 cd /opt/licode/scripts
 ./initLicode.sh
+
+cp $ROOT/erizo_controller/erizoClient/dist/erizo.js $EXTRAS/basic_example/public/
+cp $ROOT/nuve/nuveClient/dist/nuve.js $EXTRAS/basic_example/
+
+. $NVM_CHECK
+nvm use
 cd /opt/licode/extras/basic_example
 node basicServer.js
