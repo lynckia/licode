44bdbeed667b186a40985fc0d7e1515d9786e7e4
Dominik Sokal <dominiksokal@gmail.com>
2017-07-28 15:09:56 +0200

    Allow for multiple instances of erizo controller on the same host.

--- erizo_controller/erizoController/ecosystem.config.js
+++ erizo_controller/erizoController/ecosystem.config.js
@@ -7,6 +7,8 @@ module.exports = {
       script: 'erizoController.js',
       out_file: 'logs/out.log',
       error_file: 'logs/error.log',
+      instance_var: 'NODE_APP_INSTANCE',
+      instances: Number(process.env.ERIZO_CONTROLLER_INSTANCES),
     },
   ],
 };
