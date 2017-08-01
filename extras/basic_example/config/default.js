6a2f4380f1a7cf6a12bcaaf5854685cf9c5d1f71
Dominik Sokal <dominiksokal@gmail.com>
2017-08-01 11:52:17 +0200

    Fix nuve endpoint.

--- extras/basic_example/config/default.js
+++ extras/basic_example/config/default.js
@@ -6,6 +6,6 @@ var config = {};
 config.nuve = {};
 config.nuve.superserviceID = ''; // default value: ''
 config.nuve.superserviceKey = ''; // default value: ''
-config.nuve.nuveEndpoint = 'nuve-lb.service.consul:3000';
+config.nuve.nuveEndpoint = 'http://nuve-lb.service.consul:3000/';
 
 module.exports = config;
