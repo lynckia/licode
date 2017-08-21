5fda69f1f45b56c741bd071ca915c79643bb76f1
Dominik Sokal <dominiksokal@gmail.com>
2017-08-21 10:41:11 +0200

    Fix policy for choosing erizo controller.

--- nuve/nuveAPI/ch_policies/default_policy.js
+++ nuve/nuveAPI/ch_policies/default_policy.js
@@ -3,6 +3,6 @@
 var _ = require('lodash');
 
 exports.getErizoController = function (room, ecQueue) {
-  var sortedByRooms = _.sortBy(ecQueue, ({ rooms }) => rooms);
+  var sortedByRooms = _.sortBy(ecQueue, ({ roomsCount }) => roomsCount);
   return sortedByRooms[0];
 };
