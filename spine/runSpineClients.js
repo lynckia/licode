76a91fc252a1207717206b90230b15ef4fa273dd
Pedro Rodriguez <lodoyun@gmail.com>
2017-08-23 18:08:24 +0200

    Adapt Spine to ES6

--- spine/runSpineClients.js
+++ spine/runSpineClients.js
@@ -1,105 +1,42 @@
-'use strict';
-var Getopt = require('node-getopt');
-var efc = require ('./simpleNativeConnection');
+const Getopt = require('node-getopt'); // eslint-disable-line
+const Spine = require('./Spine.js')
 
-var getopt = new Getopt([
-  ['s' , 'stream-config=ARG'             , 'file containing the stream config JSON'],
-  ['t' , 'time=ARG'                      , 'interval time to show stats (default 10 seconds)'],
-  ['h' , 'help'                          , 'display this help']
+const getopt = new Getopt([
+  ['s', 'stream-config=ARG', 'file containing the stream config JSON'],
+  ['t', 'time=ARG', 'interval time to show stats (default 10 seconds)'],
+  ['h', 'help', 'display this help'],
 ]);
+const opt = getopt.parse(process.argv.slice(2));
 
-var opt = getopt.parse(process.argv.slice(2));
+let streamConfig;
+let statsInterval = 10000;
+const optionKeys = Object.keys(opt.options);
 
-var streamConfig;
+optionKeys.forEach((key) => {
+  const value = opt.options[key];
+  switch (key) {
+    case 'help':
+      getopt.showHelp();
+      process.exit(0);
+      break;
+    case 'stream-config':
+      streamConfig = value;
+      break;
+    case 'time':
+      statsInterval = value * 1000;
+      break;
+    default:
+      console.log('Default');
+      break;
+  }
+});
 
-var statsInterval = 10000;
-
-for (var prop in opt.options) {
-    if (opt.options.hasOwnProperty(prop)) {
-        var value = opt.options[prop];
-        switch (prop) {
-            case 'help':
-                getopt.showHelp();
-                process.exit(0);
-                break;
-            case 'stream-config':
-                streamConfig = value;
-                break;
-            case 'time':
-                statsInterval = value * 1000;
-                break;
-            default:
-                console.log('Default');
-                break;
-        }
-    }
-}
-
-if (!streamConfig){
-    streamConfig = 'spineClientsConfig.json';
+if (!streamConfig) {
+  streamConfig = 'spineClientsConfig.json';
 }
 
 console.log('Loading stream config file', streamConfig);
-streamConfig = require('./' + streamConfig);
+streamConfig = require(`./${streamConfig}`); // eslint-disable-line
 
-if (streamConfig.publishConfig){
-    var streamPublishConfig = {
-        publishConfig: streamConfig.publishConfig,
-        serverUrl: streamConfig.basicExampleUrl
-    };
-
-    if (streamConfig.publishersAreSubscribers){
-        streamPublishConfig.subscribeConfig = streamConfig.subscribeConfig;
-    }
-}
-
-if (streamConfig.subscribeConfig){
-    var streamSubscribeConfig = {
-        subscribeConfig : streamConfig.subscribeConfig,
-        serverUrl : streamConfig.basicExampleUrl
-    };
-    console.log('StreamSubscribe', streamSubscribeConfig);
-}
-
-var streams = [];
-
-var startStreams = function(stConf, num, time){
-    var started = 0;
-    var interval = setInterval(function(){
-        if(++started>=num){
-            console.log('All streams have been started');
-            clearInterval(interval);
-        }
-        console.log('Will start stream with config', stConf);
-        streams.push(efc.ErizoSimpleNativeConnection (stConf, function(msg){
-            console.log('Getting Callback', msg);
-        }, function(msg){
-            console.error('Error message', msg);
-        }));
-    }, time);
-};
-
-setInterval(function() {
-  var up = 0;
-  var down = 0;
-  for (var stream of streams) {
-    if (stream.getStatus() === 'connected') {
-      up++;
-    } else {
-      down++;
-    }
-  }
-  console.log('[STATS] up:', up, '; down:', down);
-}, statsInterval);
-
-console.log('Starting ', streamConfig.numSubscribers, 'subscriber streams',
-        'and', streamConfig.numPublishers, 'publisherStreams');
-
-if (streamSubscribeConfig && streamConfig.numSubscribers)
-    startStreams(streamSubscribeConfig,
-                 streamConfig.numSubscribers,
-                 streamConfig.connectionCreationInterval);
-if (streamPublishConfig && streamConfig.numPublishers)
-    startStreams(streamPublishConfig,
-                 streamConfig.numPublishers,
-                 streamConfig.connectionCreationInterval);
+const SpineClient = Spine.buildSpine(streamConfig);
+SpineClient.run();
--- spine/simpleNativeConnection.js
+++ spine/simpleNativeConnection.js
@@ -1,157 +1,175 @@
-'use strict';
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';  // We need this for testing with self signed certs
 
-var XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest,
-    nodeUrl = require ('url'),
-    Erizo = require('./erizofc'),
-    NativeStream = require ('./NativeStream.js'),
-    NativeStack = require ('./NativeStack.js');
+const XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest; // eslint-disable-line import/no-extraneous-dependencies
+const newIo = require('socket.io-client'); // eslint-disable-line import/no-extraneous-dependencies
+const nodeUrl = require('url');
+const Erizo = require('./erizofc');
+const NativeStream = require('./NativeStream.js');
+const NativeStack = require('./NativeStack.js');
+const logger = require('./logger').logger;
 
-const newIo = require('socket.io-client');
 const fakeConnection = NativeStack;
-var logger = require('./logger').logger;
-var log = logger.getLogger('ErizoSimpleNativeConnection');
+const log = logger.getLogger('ErizoSimpleNativeConnection');
 
 
-exports.ErizoSimpleNativeConnection = function (spec, callback, error){
-    var that = {};
+exports.ErizoSimpleNativeConnection = (spec, callback, error) => {
+  const that = {};
 
-    var localStream = {};
-    var subscribedStreams = [];
-    var room = '';
-    that.isActive = false;
-    localStream.getID = function() {return 0;};
-    var createToken = function(userName, role, callback) {
+  let localStream = {};
+  const subscribedStreams = new Map();
 
-        var req = new XMLHttpRequest();
-        var theUrl = nodeUrl.parse(spec.serverUrl, true);
-        if (theUrl.protocol !== 'https')
-            log.warn('Protocol is not https');
+  let room = '';
+  that.isActive = false;
+  localStream.getID = () => 0;
+  const createToken = (userName, role, tokenCallback) => {
+    const req = new XMLHttpRequest();
+    const theUrl = nodeUrl.parse(spec.serverUrl, true);
+    if (theUrl.protocol !== 'https') {
+      log.warn('Protocol is not https');
+    }
 
-        if (theUrl.query.room){
-            room = theUrl.query.room;
-            log.info('will Connect to Room', room);
-        }
+    if (theUrl.query.room) {
+      room = theUrl.query.room;
+      log.info('will Connect to Room', room);
+    }
 
-        theUrl.pathname = theUrl.pathname + 'createToken/';
+    theUrl.pathname += 'createToken/';
 
-        var url = nodeUrl.format(theUrl);
-        log.info('Url to createToken', url);
-        var body = {username: userName, role: role};
+    const url = nodeUrl.format(theUrl);
+    log.info('Url to createToken', url);
+    const body = { username: userName, role };
 
-        req.onreadystatechange = function () {
-            if (req.readyState === 4) {
-                if (req.status===200){
-                    callback(req.responseText);
-                }else{
-                    log.error('Could not get token');
-                    callback('error');
-                }
-            }
-        };
-
-        req.open('POST', url, true);
-        req.setRequestHeader('Content-Type', 'application/json');
-        req.send(JSON.stringify(body));
-    };
-
-    var subscribeToStreams = function(streams){
-        for (var index in streams){
-            if (spec.subscribeConfig){
-                log.info('Should subscribe', spec.subscribeConfig);
-                room.subscribe(streams[index], spec.subscribeConfig);
-            }
-        }
-    };
-
-    var createConnection = function (){
-
-        createToken('name', 'presenter', function (token) {
-            log.info('Getting token', token);
-            if (token === 'error'){
-                error('Could not get token from nuve in ' + spec.serverUrl);
-                return;
-            }
-            room = Erizo.Room(newIo, fakeConnection, {token: token});
-
-            room.addEventListener('room-connected', function(roomEvent) {
-                log.info('Connected to room');
-                callback('room-connected');
-                that.isActive = true;
-                subscribeToStreams(roomEvent.streams);
-                if (spec.publishConfig){
-                    log.info('Will publish with config', spec.publishConfig);
-                    localStream = NativeStream.Stream(fakeConnection, spec.publishConfig);
-                    room.publish(localStream, spec.publishConfig, function(id, message){
-                        if (id === undefined){
-                            log.error('ERROR when publishing', message);
-                            error(message);
-                        }
-                    });
-                }
-            });
-
-            room.addEventListener('stream-added', function(roomEvent) {
-                log.info('stream added', roomEvent.stream.getID());
-                callback('stream-added');
-                if (roomEvent.stream.getID()!==localStream.getID()){
-                    var streams = [];
-                    streams.push(roomEvent.stream);
-                    subscribeToStreams(streams);
-                }
-
-            });
-
-            room.addEventListener('stream-removed', function(roomEvent) {
-                callback('stream-removed');
-                if (roomEvent.stream.getID()!==localStream.getID()){
-                    room.unsubscribe(roomEvent.stream);
-                    log.info('stream removed', roomEvent.stream.getID());
-                }
-
-            });
-
-            room.addEventListener('stream-subscribed', function(streamEvent) {
-                log.info('stream-subscribed');
-                callback('stream-subscribed');
-                subscribedStreams.push(streamEvent.stream);
-            });
-
-            room.addEventListener('room-error', function(roomEvent) {
-                log.error('Room Error', roomEvent.type, roomEvent.message);
-                error(roomEvent.message);
-            });
-
-            room.addEventListener('room-disconnected', function(roomEvent) {
-                log.info('Room Disconnected', roomEvent.type, roomEvent.message);
-                callback(roomEvent.type+roomEvent.message);
-            });
-
-            room.connect();
-        });
-
-        that.sendData = function(){
-            localStream.sendData({msg: 'Testing Data Connection'});
-        };
-    };
-
-    that.close = function(){
-        log.info('Close');
-        room.disconnect();
-    };
-
-    that.getStatus = function() {
-      if (subscribedStreams.length === 0) {
-        return 'disconnected';
-      }
-      for (var stream of subscribedStreams) {
-        if (!stream.pc || !stream.pc.peerConnection || !stream.pc.peerConnection.connected) {
-          return 'disconnected';
+    req.onreadystatechange = () => {
+      if (req.readyState === 4) {
+        if (req.status === 200) {
+          tokenCallback(req.responseText);
+        } else {
+          log.error('Could not get token');
+          tokenCallback('error');
         }
       }
-      return 'connected';
     };
 
-    createConnection();
-    return that;
+    req.open('POST', url, true);
+    req.setRequestHeader('Content-Type', 'application/json');
+    req.send(JSON.stringify(body));
+  };
+
+  const subscribeToStreams = (streams) => {
+    streams.forEach((stream) => {
+      if (spec.subscribeConfig) {
+        log.info('Should subscribe', spec.subscribeConfig);
+        room.subscribe(stream, spec.subscribeConfig);
+      }
+    });
+  };
+
+  const createConnection = () => {
+    createToken('name', 'presenter', (token) => {
+      log.info('Getting token', token);
+      if (token === 'error') {
+        error(`Could not get token from nuve in ${spec.serverUrl}`);
+        return;
+      }
+      room = Erizo.Room(newIo, fakeConnection, { token });
+
+      room.addEventListener('room-connected', (roomEvent) => {
+        log.info('Connected to room');
+        callback('room-connected');
+        that.isActive = true;
+        subscribeToStreams(roomEvent.streams);
+        if (spec.publishConfig) {
+          log.info('Will publish with config', spec.publishConfig);
+          localStream = NativeStream.Stream(fakeConnection, spec.publishConfig);
+          room.publish(localStream, spec.publishConfig, (id, message) => {
+            if (id === undefined) {
+              log.error('ERROR when publishing', message);
+              error(message);
+            }
+          });
+        }
+      });
+
+      room.addEventListener('stream-added', (roomEvent) => {
+        log.info('stream added', roomEvent.stream.getID());
+        callback('stream-added');
+        if (roomEvent.stream.getID() === localStream.getID()) {
+          localStream.published = true;
+        } else {
+          const streams = [];
+          streams.push(roomEvent.stream);
+          subscribeToStreams(streams);
+        }
+      });
+
+      room.addEventListener('stream-removed', (roomEvent) => {
+        callback('stream-removed');
+        const eventStreamId = roomEvent.stream.getID();
+        if (eventStreamId === localStream.getID()) {
+          localStream.published = false;
+        } else {
+          room.unsubscribe(roomEvent.stream);
+          subscribedStreams.delete(eventStreamId);
+          log.info('stream removed', eventStreamId);
+        }
+      });
+
+      room.addEventListener('stream-subscribed', (streamEvent) => {
+        log.info('stream-subscribed');
+        callback('stream-subscribed');
+        subscribedStreams.set(streamEvent.stream.getID(), streamEvent.stream);
+      });
+
+      room.addEventListener('room-error', (roomEvent) => {
+        log.error('Room Error', roomEvent.type, roomEvent.message);
+        error(roomEvent.message);
+      });
+
+      room.addEventListener('room-disconnected', (roomEvent) => {
+        log.info('Room Disconnected', roomEvent.type, roomEvent.message);
+        callback(roomEvent.type + roomEvent.message);
+      });
+
+      room.connect();
+    });
+
+    that.sendData = () => {
+      localStream.sendData({ msg: 'Testing Data Connection' });
+    };
+  };
+
+  that.close = () => {
+    log.info('Close');
+    room.disconnect();
+  };
+
+  that.getStats = () => {
+    const promises = [];
+    subscribedStreams.forEach((stream, streamId) => {
+      if (stream.pc && stream.pc.peerConnection) {
+        promises.push(stream.pc.peerConnection.getStats());
+      } else {
+        log.info('No stream to ask for stats: ', streamId);
+        promises.push('failed');
+      }
+    });
+    return Promise.all(promises);
+  };
+
+  that.getStatus = () => {
+    if (subscribedStreams.length === 0) {
+      return ['disconnected'];
+    }
+    const subscriptionStatus = [];
+    subscribedStreams.values.forEach((stream) => {
+      if (!stream.pc || !stream.pc.peerConnection || !stream.pc.peerConnection.connected) {
+        subscriptionStatus.push('disconnected');
+      }
+      subscriptionStatus.push('connected');
+    });
+    return subscriptionStatus;
+  };
+
+  createConnection();
+  return that;
 };
