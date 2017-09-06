5e8c25960ab97151ddae100c9698418325df9bc2
Marek Kaput <marek.kaput@swmansion.com>
2017-09-06 08:47:03 +0200

    minor, use builtin getter

--- erizo_controller/erizoJS/erizoJSController.js
+++ erizo_controller/erizoJS/erizoJSController.js
@@ -45,8 +45,7 @@ exports.ErizoJSController = function (erizoAgentID, erizoJSID, threadPool, ioThr
     const updateStats = () => {
         const publishersKeys = Object.keys(publishers);
         const publishersCount = publishersKeys.length;
-        const subscribersCounts = publishersKeys.map(key =>
-            Object.keys(publishers[key].subscribers).length);
+        const subscribersCounts = publishersKeys.map(key => publishers[key].numSubscribers);
         const subscribersCount = subscribersCounts.reduce((acc, i) => acc + i, 0);
 
         db.erizoJS.update(
