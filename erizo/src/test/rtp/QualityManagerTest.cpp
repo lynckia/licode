ef1294f40670616149c9300543801caa575fdeff
Javier Cerviño <jcague@gmail.com>
2017-09-19 13:27:25 +0200

    Make QualityManager decide the best layer upon constraints (#1035)

--- erizo/src/test/rtp/QualityManagerTest.cpp
+++ erizo/src/test/rtp/QualityManagerTest.cpp
@@ -7,6 +7,7 @@
 #include <stats/StatNode.h>
 #include <MediaDefinitions.h>
 #include <WebRtcConnection.h>
+#include <rtp/LayerDetectorHandler.h>
 
 #include <string>
 #include <vector>
@@ -28,6 +29,7 @@ using erizo::MovingIntervalRateStat;
 using erizo::CumulativeStat;
 using erizo::Pipeline;
 using erizo::duration;
+using erizo::LayerInfoChangedEvent;
 
 const int kBaseSpatialLayer = 0;
 const int kBaseTemporalLayer = 0;
@@ -39,14 +41,20 @@ const uint32_t kRateStatIntervals = 10;
 const int kArbitraryNumberOfSpatialLayers = 2;
 const int kArbitraryNumberOfTemporalLayers = 3;
 
+const int kVideoWidthSL1 = 640;
+const int kVideoHeightSL1 = 480;
+const int kVideoWidthSL0 = 320;
+const int kVideoHeightSL0 = 240;
+const int kFrameRateTL2 = 30;
+const int kFrameRateTL1 = 15;
+const int kFrameRateTL0 = 5;
 
-class QualityManagerTest : public erizo::HandlerTest{
+class QualityManagerBaseTest : public erizo::BaseHandlerTest{
  public:
-  QualityManagerTest(): stats_clock{std::make_shared<SimulatedClock>()} {
+  QualityManagerBaseTest(): stats_clock{std::make_shared<SimulatedClock>()} {
   }
 
- protected:
-  void setHandler() {
+  void internalSetHandler() {
     quality_manager = std::make_shared<QualityManager>(simulated_clock);
     pipeline->addService(quality_manager);
     generateLayersWithGrowingBitrate(kArbitraryNumberOfSpatialLayers, kArbitraryNumberOfTemporalLayers);
@@ -86,6 +94,30 @@ class QualityManagerTest : public erizo::HandlerTest{
     stats->getNode()["total"].insertStat("senderBitrateEstimation", CumulativeStat{bitrate});
   }
 
+  void notifyLayerInfo() {
+    std::vector<uint32_t> video_frame_width_list;
+    std::vector<uint32_t> video_frame_height_list;
+    std::vector<uint64_t> video_frame_rate_list;
+
+    video_frame_width_list.push_back(kVideoWidthSL0);
+    video_frame_height_list.push_back(kVideoHeightSL0);
+
+    video_frame_width_list.push_back(kVideoWidthSL1);
+    video_frame_height_list.push_back(kVideoHeightSL1);
+
+    video_frame_rate_list.push_back(kFrameRateTL0);
+    video_frame_rate_list.push_back(kFrameRateTL1);
+    video_frame_rate_list.push_back(kFrameRateTL2);
+
+    auto event = std::make_shared<LayerInfoChangedEvent>(video_frame_width_list,
+      video_frame_height_list, video_frame_rate_list);
+    quality_manager->notifyEvent(event);
+  }
+
+  void setVideoConstraints(int max_width, int max_height, int max_frame_rate) {
+    quality_manager->setVideoConstraints(max_width, max_height, max_frame_rate);
+  }
+
   void advanceClock(duration time) {
     simulated_clock->advanceTime(time);
   }
@@ -96,6 +128,24 @@ class QualityManagerTest : public erizo::HandlerTest{
 
   std::shared_ptr<QualityManager> quality_manager;
   std::shared_ptr<SimulatedClock> stats_clock;
+};
+
+class QualityManagerTest : public ::testing::Test, public QualityManagerBaseTest {
+ public:
+  QualityManagerTest() {}
+
+  void setHandler() override {
+    internalSetHandler();
+  }
+
+ protected:
+  void SetUp() override {
+    internalSetUp();
+  }
+
+  void TearDown() override {
+    internalTearDown();
+  }
 };
 
 TEST_F(QualityManagerTest, shouldStartAtBaseLayer) {
@@ -202,3 +252,129 @@ TEST_F(QualityManagerTest, shouldStickToForcedLayer) {
   EXPECT_EQ(quality_manager->getSpatialLayer() , kBaseSpatialLayer);
   EXPECT_EQ(quality_manager->getTemporalLayer() , kBaseTemporalLayer);
 }
+
+class QualityManagerConstraintsTest : public QualityManagerBaseTest,
+                           public ::testing::TestWithParam<std::tr1::tuple<int, int, int, int, int>> {
+ public:
+  QualityManagerConstraintsTest() {
+    max_video_width = std::tr1::get<0>(GetParam());
+    max_video_height = std::tr1::get<1>(GetParam());
+    max_frame_rate = std::tr1::get<2>(GetParam());
+    expected_spatial_layer = std::tr1::get<3>(GetParam());
+    expected_temporal_layer = std::tr1::get<4>(GetParam());
+  }
+
+  void setHandler() override {
+    internalSetHandler();
+  }
+
+ protected:
+  void SetUp() override {
+    internalSetUp();
+  }
+
+  void TearDown() override {
+    internalTearDown();
+  }
+  int max_video_width;
+  int max_video_height;
+  int max_frame_rate;
+  int expected_spatial_layer;
+  int expected_temporal_layer;
+};
+
+TEST_P(QualityManagerConstraintsTest, shouldChooseCorrectLayersBasedOnConstraints) {
+  quality_manager->notifyQualityUpdate();
+  const int kArbitrarySpatialLayer = 1;
+  const int kArbitraryTemporalLayer = 2;
+  advanceClock(erizo::QualityManager::kMinLayerSwitchInterval + std::chrono::milliseconds(1));
+
+  float margin = 1. + QualityManager::kIncreaseLayerBitrateThreshold;
+
+  setSenderBitrateEstimation(getStatForLayer(kArbitrarySpatialLayer, kArbitraryTemporalLayer) * margin + 1);
+  notifyLayerInfo();
+  quality_manager->notifyQualityUpdate();
+
+  setVideoConstraints(max_video_width,
+                      max_video_height,
+                      max_frame_rate);
+
+  EXPECT_EQ(quality_manager->getSpatialLayer() , expected_spatial_layer);
+  EXPECT_EQ(quality_manager->getTemporalLayer() , expected_temporal_layer);
+}
+
+INSTANTIATE_TEST_CASE_P(
+  QualityManagerConstraints1, QualityManagerConstraintsTest, testing::Values(
+    //                 video width,    video height,    frame rate, expected SL, expected TL
+    std::make_tuple(kVideoWidthSL1, kVideoHeightSL1, kFrameRateTL2,           1,           2),
+    std::make_tuple(kVideoWidthSL1, kVideoHeightSL1, kFrameRateTL1,           1,           1),
+    std::make_tuple(kVideoWidthSL1, kVideoHeightSL1, kFrameRateTL0,           1,           0),
+    std::make_tuple(kVideoWidthSL1, kVideoHeightSL1,             0,           1,           0),
+
+    std::make_tuple(kVideoWidthSL0, kVideoHeightSL0, kFrameRateTL2,           0,           2),
+    std::make_tuple(kVideoWidthSL0, kVideoHeightSL0, kFrameRateTL1,           0,           1),
+    std::make_tuple(kVideoWidthSL0, kVideoHeightSL0, kFrameRateTL0,           0,           0),
+    std::make_tuple(kVideoWidthSL0, kVideoHeightSL0,             0,           0,           0),
+
+    std::make_tuple(0,                            0, kFrameRateTL2,           0,           2),
+    std::make_tuple(0,                            0, kFrameRateTL1,           0,           1),
+    std::make_tuple(0,                            0, kFrameRateTL0,           0,           0),
+    std::make_tuple(0,                            0,             0,           0,           0),
+
+    std::make_tuple(kVideoWidthSL1,              -1, kFrameRateTL2,           1,           2),
+    std::make_tuple(kVideoWidthSL1,              -1, kFrameRateTL1,           1,           1),
+    std::make_tuple(kVideoWidthSL1,              -1, kFrameRateTL0,           1,           0),
+    std::make_tuple(kVideoWidthSL1,              -1,             0,           1,           0),
+
+    std::make_tuple(-1,             kVideoHeightSL1, kFrameRateTL2,           1,           2),
+    std::make_tuple(-1,             kVideoHeightSL1, kFrameRateTL1,           1,           1),
+    std::make_tuple(-1,             kVideoHeightSL1, kFrameRateTL0,           1,           0),
+    std::make_tuple(-1,             kVideoHeightSL1,             0,           1,           0),
+
+    std::make_tuple(-1,                          -1, kFrameRateTL2,           1,           2),
+    std::make_tuple(-1,                          -1, kFrameRateTL1,           1,           1),
+    std::make_tuple(-1,                          -1, kFrameRateTL0,           1,           0),
+    std::make_tuple(-1,                          -1,             0,           1,           0)));
+
+INSTANTIATE_TEST_CASE_P(
+  QualityManagerConstraints2, QualityManagerConstraintsTest, testing::Values(
+    //                 video width,    video height,    frame rate, expected SL, expected TL
+    std::make_tuple(kVideoWidthSL0, kVideoHeightSL1, kFrameRateTL2,           1,           2),
+    std::make_tuple(0,              kVideoHeightSL1, kFrameRateTL2,           1,           2),
+    std::make_tuple(-1,             kVideoHeightSL0, kFrameRateTL2,           0,           2),
+    std::make_tuple(kVideoWidthSL1, kVideoHeightSL0, kFrameRateTL2,           1,           2),
+    std::make_tuple(kVideoWidthSL1,               0, kFrameRateTL2,           1,           2),
+    std::make_tuple(kVideoWidthSL0,               0, kFrameRateTL2,           0,           2),
+    std::make_tuple(kVideoWidthSL0,              -1, kFrameRateTL2,           0,           2),
+
+    std::make_tuple(kVideoWidthSL0, kVideoHeightSL1, kFrameRateTL1,           1,           1),
+    std::make_tuple(0,              kVideoHeightSL1, kFrameRateTL1,           1,           1),
+    std::make_tuple(-1,             kVideoHeightSL0, kFrameRateTL1,           0,           1),
+    std::make_tuple(kVideoWidthSL1, kVideoHeightSL0, kFrameRateTL1,           1,           1),
+    std::make_tuple(kVideoWidthSL1,               0, kFrameRateTL1,           1,           1),
+    std::make_tuple(kVideoWidthSL0,               0, kFrameRateTL1,           0,           1),
+    std::make_tuple(kVideoWidthSL0,              -1, kFrameRateTL1,           0,           1),
+
+    std::make_tuple(kVideoWidthSL0, kVideoHeightSL1, kFrameRateTL0,           1,           0),
+    std::make_tuple(0,              kVideoHeightSL1, kFrameRateTL0,           1,           0),
+    std::make_tuple(-1,             kVideoHeightSL0, kFrameRateTL0,           0,           0),
+    std::make_tuple(kVideoWidthSL1, kVideoHeightSL0, kFrameRateTL0,           1,           0),
+    std::make_tuple(kVideoWidthSL1,               0, kFrameRateTL0,           1,           0),
+    std::make_tuple(kVideoWidthSL0,              -1, kFrameRateTL0,           0,           0),
+
+    std::make_tuple(kVideoWidthSL0, kVideoHeightSL1,             0,           1,           0),
+    std::make_tuple(0,              kVideoHeightSL1,             0,           1,           0),
+    std::make_tuple(-1,             kVideoHeightSL0,             0,           0,           0),
+    std::make_tuple(kVideoWidthSL1, kVideoHeightSL0,             0,           1,           0),
+    std::make_tuple(kVideoWidthSL1,               0,             0,           1,           0),
+    std::make_tuple(kVideoWidthSL0,              -1,             0,           0,           0),
+
+    std::make_tuple(kVideoWidthSL0, kVideoHeightSL1,            -1,           1,           2),
+    std::make_tuple(0,              kVideoHeightSL1,            -1,           1,           2),
+    std::make_tuple(-1,             kVideoHeightSL0,            -1,           0,           2),
+    std::make_tuple(kVideoWidthSL1, kVideoHeightSL0,            -1,           1,           2),
+    std::make_tuple(kVideoWidthSL1,               0,            -1,           1,           2),
+    std::make_tuple(kVideoWidthSL0,              -1,            -1,           0,           2),
+
+    std::make_tuple(kVideoWidthSL1+1, kVideoHeightSL1+1, kFrameRateTL2+1,     1,           2),
+    std::make_tuple(-1,                          -1,            -1,           1,           2)));
