9ce2b51228892f7a32f91da97dc093b3634465d0
Javier Cerviño <jcague@gmail.com>
2017-12-12 10:42:20 +0100

    Set default simulcast layers for Firefox.

--- erizo_controller/erizoClient/src/webrtc-stacks/FirefoxStack.js
+++ erizo_controller/erizoClient/src/webrtc-stacks/FirefoxStack.js
@@ -1,28 +1,39 @@
 import Logger from '../utils/Logger';
 import BaseStack from './BaseStack';
 
+const possibleLayers = [
+  { rid: 'high' },
+  { rid: 'med', scaleResolutionDownBy: 2 },
+  { rid: 'low', scaleResolutionDownBy: 3 },
+];
+
 const FirefoxStack = (specInput) => {
   Logger.info('Starting Firefox stack');
   const that = BaseStack(specInput);
+  const spec = specInput;
+  const defaultSimulcastSpatialLayers = 2;
+
+  const getSimulcastParameters = (sender) => {
+    let numSpatialLayers = spec.simulcast.numSpatialLayers || defaultSimulcastSpatialLayers;
+    const totalLayers = possibleLayers.length;
+    numSpatialLayers = numSpatialLayers < totalLayers ?
+                          numSpatialLayers : totalLayers;
+    const parameters = sender.getParameters() || {};
+    parameters.encodings = [];
+
+    for (let layer = totalLayers - numSpatialLayers; layer < totalLayers; layer += 1) {
+      parameters.encodings.push(possibleLayers[layer]);
+    }
+    sender.setParameters(parameters);
+  };
 
   const enableSimulcast = () => {
+    if (!spec.simulcast) {
+      return;
+    }
     that.peerConnection.getSenders().forEach((sender) => {
       if (sender.track.kind === 'video') {
-        sender.getParameters();
-        sender.setParameters({ encodings: [{
-          rid: 'spam',
-          active: true,
-          priority: 'high',
-          maxBitrate: 600000,
-          maxHeight: 640,
-          maxWidth: 480 }, {
-            rid: 'egg',
-            active: true,
-            priority: 'medium',
-            maxBitrate: 600000,
-            maxHeight: 320,
-            maxWidth: 240 }],
-        });
+        getSimulcastParameters(sender);
       }
     });
   };
