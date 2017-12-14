606674984587e4078723a7bd64b23e6f9fa9b030
Andrea <trisfald@gmail.com>
2017-12-14 11:24:10 +0100

    log macro can accept std::string

--- erizo/src/erizo/logger.h
+++ erizo/src/erizo/logger.h
@@ -28,6 +28,7 @@
 #include <map>
 #include <string>
 #include <utility>
+#include <type_traits>
 
 class LogContext {
  public:
@@ -114,23 +115,64 @@ if (logger->isFatalEnabled()) { \
   LOG4CXX_FATAL(logger, __tmp); \
 }
 
+namespace detail {
+// Helper for forwarding correctly the object to be logged
+template <typename T>
+struct LogElementForwarder {
+  T operator()(T t) {
+    return t;
+  }
+};
+
+template <>
+struct LogElementForwarder<std::string> {
+  template <typename S>
+  const char* operator()(const S& t) {
+    return t.c_str();
+  }
+};
+}  // namespace detail
+
+#define DEFINE_ELOG_T(name, invoke) \
+template <typename Logger, typename... Args> \
+inline void name(const Logger&, const char*, Args...) __attribute__((always_inline)); \
+\
+template <typename Logger, typename... Args> \
+void name(const Logger& logger, const char* fmt, Args... args) { \
+  invoke(logger, fmt, detail::LogElementForwarder<typename std::decay<Args>::type>{}(args)...); \
+} \
+\
+template <typename Logger> \
+inline void name(const Logger&, const char*) __attribute__((always_inline)); \
+\
+template <typename Logger> \
+void name(const Logger& logger, const char* fmt) { \
+  invoke(logger, "%s", fmt); \
+}
+
+DEFINE_ELOG_T(ELOG_TRACET, ELOG_TRACE2)
+DEFINE_ELOG_T(ELOG_DEBUGT, ELOG_DEBUG2)
+DEFINE_ELOG_T(ELOG_INFOT, ELOG_INFO2)
+DEFINE_ELOG_T(ELOG_WARNT, ELOG_WARN2)
+DEFINE_ELOG_T(ELOG_ERRORT, ELOG_ERROR2)
+DEFINE_ELOG_T(ELOG_FATALT, ELOG_FATAL2)
 
 #define ELOG_TRACE(fmt, args...) \
-ELOG_TRACE2(logger, fmt, ##args);
+ELOG_TRACET(logger, fmt, ##args);
 
 #define ELOG_DEBUG(fmt, args...) \
-ELOG_DEBUG2(logger, fmt, ##args);
+ELOG_DEBUGT(logger, fmt, ##args);
 
 #define ELOG_INFO(fmt, args...) \
-ELOG_INFO2(logger, fmt, ##args);
+ELOG_INFOT(logger, fmt, ##args);
 
 #define ELOG_WARN(fmt, args...) \
-ELOG_WARN2(logger, fmt, ##args);
+ELOG_WARNT(logger, fmt, ##args);
 
 #define ELOG_ERROR(fmt, args...) \
-ELOG_ERROR2(logger, fmt, ##args);
+ELOG_ERRORT(logger, fmt, ##args);
 
 #define ELOG_FATAL(fmt, args...) \
-ELOG_FATAL2(logger, fmt, ##args);
+ELOG_FATALT(logger, fmt, ##args);
 
 #endif  // ERIZO_SRC_ERIZO_LOGGER_H_
