b4bb832c68075f69da3217b38e568ea68cf30cc3
Pedro Rodriguez <lodoyun@gmail.com>
2018-03-21 16:22:42 +0100

    Fix/erizo client build errors (#1171)

--- erizo_controller/erizoClient/src/ErizoConnectionManager.js
+++ erizo_controller/erizoClient/src/ErizoConnectionManager.js
@@ -7,10 +7,10 @@ import { EventEmitter, ConnectionEvent } from './Events';
 import ErizoMap from './utils/ErizoMap';
 import ConnectionHelpers from './utils/ConnectionHelpers';
 
-
+const EventEmitterConst = EventEmitter; // makes google-closure-compiler happy
 let ErizoSessionId = 103;
 
-class ErizoConnection extends EventEmitter {
+class ErizoConnection extends EventEmitterConst {
   constructor(specInput, erizoId = undefined) {
     super();
     Logger.debug('Building a new Connection');
