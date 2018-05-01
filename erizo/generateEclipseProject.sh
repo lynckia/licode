b8eb85cb75c9f5191f9b17ac112160a643ef460e
Francesco Durighetto <work@francescodurighetto.com>
2018-05-01 14:14:42 +0200

    fix geneateEclipseProject

--- erizo/generateEclipseProject.sh
+++ erizo/generateEclipseProject.sh
@@ -6,7 +6,7 @@ BIN_DIR="build"
 if [ -d $BIN_DIR ]; then
   cd $BIN_DIR
   # Set to Debug to be able to debug in Eclipse
-  cmake -G"Eclipse CDT4 - Unix Makefiles" -D CMAKE_BUILD_TYPE=Debug ../src
+  cmake -G"Eclipse CDT4 - Unix Makefiles" -D ERIZO_BUILD_TYPE=debug ../src
   echo "Done"
   cd ..
 else
