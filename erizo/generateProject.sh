91940cce12c9f56396996cfe84b418ed4eef411f
Francesco Durighetto <work@francescodurighetto.com>
2018-06-04 10:24:49 +0200

    revert to run cmake every time

--- erizo/generateProject.sh
+++ erizo/generateProject.sh
@@ -10,11 +10,13 @@ BASE_BIN_DIR="build"
 generateVersion() {
   echo "generating $1"
   BIN_DIR="$BASE_BIN_DIR/$1"
-  if [ ! -d $BIN_DIR ]; then
+  if [ -d $BIN_DIR ]; then
+    cd $BIN_DIR
+  else
     mkdir -p $BIN_DIR
     cd $BIN_DIR
-    cmake ../../src "-DERIZO_BUILD_TYPE=$1"
   fi
+  cmake ../../src "-DERIZO_BUILD_TYPE=$1"
   cd $PATHNAME
 }
 
