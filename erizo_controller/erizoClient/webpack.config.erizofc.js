cc8bf0d3ac3bfc4fe22428d5da59c91a5a5ff3ed
Francesco Durighetto <work@francescodurighetto.com>
2018-06-08 09:56:31 +0200

    ErizoFc compiled as UMD

--- erizo_controller/erizoClient/webpack.config.erizofc.js
+++ erizo_controller/erizoClient/webpack.config.erizofc.js
@@ -7,7 +7,7 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     libraryExport: 'default',
     library: 'Erizo',
-    libraryTarget: 'var',
+    libraryTarget: 'umd',
   },
   module: {
     rules: [{
