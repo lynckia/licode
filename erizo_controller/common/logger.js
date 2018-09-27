74c0489bedc0d944ead5a737d06060cc29df653f
Pedro Rodriguez <lodoyun@gmail.com>
2018-09-27 12:00:09 +0200

    Translated erizoController to eslint

--- erizo_controller/common/logger.js
+++ erizo_controller/common/logger.js
@@ -1,40 +1,40 @@
-'use strict';
-var log4js = require('log4js');
 
-global.config = global.config || {};
+// eslint-disable-next-line import/no-extraneous-dependencies
+const log4js = require('log4js');
+
+global.config = global.config || {};
 
 global.config.logger = global.config.logger || {};
 
-var logFile = global.config.logger.configFile ||  '../log4js_configuration.json';
+const logFile = global.config.logger.configFile || '../log4js_configuration.json';
 
 
-var logJsonReplacer = function (key, value){
-    if (key){
-        if (typeof(value) === 'object'){
-            return '[Object]';
-        }
-        return value;
-    }else{
-        return value;
+const logJsonReplacer = (key, value) => {
+  if (key) {
+    if (typeof (value) === 'object') {
+      return '[Object]';
     }
+    return value;
+  }
+  return value;
 };
 
 log4js.configure(logFile);
 
 exports.logger = log4js;
 
-exports.logger.objectToLog = function (jsonInput) {
-    if (jsonInput === undefined){
-        return '';
-    }
-    if (typeof(jsonInput) !== 'object') {
-        return jsonInput;
-    } else if (jsonInput.constructor === Array) {
-        return '[Object]';
-    }
-    var jsonString = JSON.stringify (jsonInput, logJsonReplacer);
-    return jsonString.replace (/['"]+/g, '')
+exports.logger.objectToLog = (jsonInput) => {
+  if (jsonInput === undefined) {
+    return '';
+  }
+  if (typeof (jsonInput) !== 'object') {
+    return jsonInput;
+  } else if (jsonInput.constructor === Array) {
+    return '[Object]';
+  }
+  const jsonString = JSON.stringify(jsonInput, logJsonReplacer);
+  return jsonString.replace(/['"]+/g, '')
                      .replace(/[:]+/g, ': ')
                      .replace(/[,]+/g, ', ')
-                     .slice(1,-1);
+                     .slice(1, -1);
 };
