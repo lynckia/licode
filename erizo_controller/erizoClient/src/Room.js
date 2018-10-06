d5768309ccf772be514716ef1a440eea942f743e
cracker <rofl256@gmail.com>
2018-10-06 14:34:07 +0200

    fix subscribing to you own streams

--- erizo_controller/erizoClient/src/Room.js
+++ erizo_controller/erizoClient/src/Room.js
@@ -232,7 +232,7 @@ const Room = (altIo, altConnectionHelpers, altConnectionManager, specInput) => {
       return;
     }
     const stream = Stream(that.Connection, { streamID: arg.id,
-      local: localStreams.has(arg.id),
+      local: false,
       audio: arg.audio,
       video: arg.video,
       data: arg.data,
