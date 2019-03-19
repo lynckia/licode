195110ea92fdc2e4358e78e284f9189a182401bf
Javier Cerviño <jcague@gmail.com>
2019-03-19 15:55:57 +0100

    Minor fix in roomController

--- erizo_controller/erizoController/roomController.js
+++ erizo_controller/erizoController/roomController.js
@@ -356,7 +356,7 @@ exports.RoomController = (spec) => {
       if (publishers[streamId] !== undefined) {
         message = 'publisher not found';
       }
-      if (subscribers[streamId] && subscribers[streamId].indexOf(clientId) === -1) {
+      if (subscribers[streamId] && subscribers[streamId].indexOf(clientId) !== -1) {
         message = 'subscriber already exists';
       }
       log.warn('message: addSubscriber can not be requested, ' +
