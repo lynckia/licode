d422755e174a51c43afa63a4c4961bb85731d9c9
Javier Cerviño <jcague@gmail.com>
2019-09-18 09:58:34 +0200

    Fix basic example port

--- extras/basic_example/basicServer.js
+++ extras/basic_example/basicServer.js
@@ -200,9 +200,18 @@ app.use((req, res, next) => {
 cleanExampleRooms(() => {
   getOrCreateRoom(defaultRoomName, undefined, undefined, (roomId) => {
     defaultRoom = roomId;
-    app.listen(config.basicExample.port || 3001);
+    let port = 3001;
+    let tlsPort = 3004;
+    if (config.basicExample && config.basicExample.port) {
+      port = config.basicExample.port;
+    }
+    if (config.basicExample && config.basicExample.tlsPort) {
+      tlsPort = config.basicExample.tlsPort;
+    }
+
+    app.listen(port);
     const server = https.createServer(options, app);
     console.log('BasicExample started');
-    server.listen(config.basicExample.tlsPort || 3004);
+    server.listen(tlsPort);
   });
 });
