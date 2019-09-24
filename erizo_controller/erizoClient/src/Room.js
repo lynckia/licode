3fd3c7f25ca6c0c66c3bb0dc8e07a8778f60787e
Pedro Rodriguez <lodoyun@gmail.com>
2019-09-24 12:30:43 +0200

    Fixed lint

--- erizo_controller/erizoClient/src/Room.js
+++ erizo_controller/erizoClient/src/Room.js
@@ -347,7 +347,7 @@ const Room = (altIo, altConnectionHelpers, altConnectionManager, specInput) => {
     } else if (arg.context === 'auto-streams-unsubscription') {
       onAutomaticStreamsUnsubscription(arg.mess);
     } else {
-      Logger.debug('Failed applying a stream message from erizo', arg);	
+      Logger.debug('Failed applying a stream message from erizo', arg);
     }
   };
 
