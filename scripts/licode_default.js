8e7a7b94d730f97e7e6282b9d2fd35d6218ea4ca
Pedro Rodriguez <lodoyun@gmail.com>
2021-01-19 11:10:35 +0100

    Change to double quotes to keep consistency

--- scripts/licode_default.js
+++ scripts/licode_default.js
@@ -23,7 +23,7 @@ config.cloudProvider.name = '';
  NUVE CONFIGURATION
 **********************************************************/
 config.nuve = {};
-config.nuve.dataBaseURL = 'mongodb://localhost/nuvedb'; // default value: 'mongodb://localhost/nuvedb'
+config.nuve.dataBaseURL = "mongodb://localhost/nuvedb"; // default value: "mongodb://localhost/nuvedb"
 config.nuve.dataBaseName = 'nuvedb'; // default value: 'nuvedb'
 config.nuve.superserviceID = '_auto_generated_ID_'; // default value: ''
 config.nuve.superserviceKey = '_auto_generated_KEY_'; // default value: ''
