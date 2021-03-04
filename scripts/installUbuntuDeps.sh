f6d73db15794bd17bfe8f3ea5355f2cdbf97ec7b
Álvaro Alonso <aalonsog@dit.upm.es>
2021-03-04 17:17:45 +0100

    Added log for mongodb installation

--- scripts/installUbuntuDeps.sh
+++ scripts/installUbuntuDeps.sh
@@ -103,6 +103,7 @@ install_apt_deps(){
 }
 
 install_mongodb(){
+  echo "Installing mongodb-org"
   wget -qO - https://www.mongodb.org/static/pgp/server-4.4.asc | sudo apt-key add -
   echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/4.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.4.list
   sudo apt-get update
