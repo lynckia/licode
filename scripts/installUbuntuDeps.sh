29486bff2ce7d342c19cd7fccade0b7b1da4e5e5
Alvaro Alonso <aalonsog@dit.upm.es>
2021-03-09 13:24:22 +0100

    Installing mongodb using tgz

--- scripts/installUbuntuDeps.sh
+++ scripts/installUbuntuDeps.sh
@@ -103,11 +103,11 @@ install_apt_deps(){
 }
 
 install_mongodb(){
-  echo "Installing mongodb-org"
-  wget -qO - https://www.mongodb.org/static/pgp/server-4.4.asc | sudo apt-key add -
-  echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/4.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.4.list
-  sudo apt-get update
-  sudo apt-get install -y mongodb-org
+  echo "Installing mongodb-org from tar"
+  sudo apt-get install -y libcurl4 openssl liblzma5
+  wget -P $LIB_DIR https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-ubuntu2004-4.4.4.tgz
+  tar -zxvf $LIB_DIR/mongodb-linux-x86_64-ubuntu2004-4.4.4.tgz -C $LIB_DIR
+  sudo cp $LIB_DIR/mongodb-linux-x86_64-ubuntu2004-4.4.4/bin/* /usr/local/bin/
 }
 
 install_conan(){
