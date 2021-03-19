265deccea6625398146a473de31846346a13c9bb
vpoddubchak <vapodd@gmail.com>
2021-03-19 16:12:12 +0200

    Use correct key

--- erizo/src/erizo/SdpInfo.cpp
+++ erizo/src/erizo/SdpInfo.cpp
@@ -131,7 +131,7 @@ namespace erizo {
     if (inOutPTMap.count(payloadType) > 0) {
       for (unsigned int it = 0; it < payloadVector.size(); it++) {
         const RtpMap& rtp = payloadVector[it];
-        if (inOutPTMap[rtp.payload_type] == payloadType) {
+        if (inOutPTMap[payloadType] == rtp.payload_type) {
           return true;
         }
       }
