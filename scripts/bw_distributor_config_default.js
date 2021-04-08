56c699f9c400dae12c7340e84790c9269ea0ce72
Pedro Rodriguez <lodoyun@gmail.com>
2021-04-08 11:54:51 +0200

    Improve default bw config

--- scripts/bw_distributor_config_default.js
+++ scripts/bw_distributor_config_default.js
@@ -1,31 +1,31 @@
 const bwDistributorConfig = {};
-bwDistributorConfig.defaultType = 'TargetVideoBW'; // Options: 'StreamPriority', 'MaxVideoBW', 'TargetVideoBW'
+bwDistributorConfig.defaultType = 'TargetVideoBW'; // BwDistributor to use when no strategy is defined for a client. Options: 'MaxVideoBW', 'TargetVideoBW'
 bwDistributorConfig.strategyDefinitions = {
   high: {
-    strategyDescription: 'Strict Stream Prioritization with Budgeted Base Layer',
+    strategyDescription: 'High Layers available',
     priorities: {
-      default: ['fallback', 'SL0', 'SL1', 'SL2'],
-      0: ['fallback', 'SL0', 'SL1', 'SL2'],
-      10: ['fallback', 'SL0', 'SL1', 'SL2'],
+      default: ['SL0', 'SL1', 'SL2'],
+      0: ['SL0', 'SL1', 'SL2'],
+      10: ['SL0', 'SL1', 'SL2'],
       20: ['slideshow', 'SL0', 'SL1', 'SL2'],
-      30: ['fallback', 'SL0', 'SL1', 'SL2'],
-      40: ['fallback', 'SL0', 'SL1', 'SL2'],
+      30: ['SL0', 'SL1', 'SL2'],
+      40: ['SL0', 'SL1', 'SL2'],
     },
     strategy: ['40_0', '30_0', '20_0', '10_0', '0_0', 'default_0', // Base levels
       '40_1', '40_2',
       '30_1', '30_2',
-      '20_1', '20_2',
+      '20_1', '20_2', '20_3',
       '10_1', '10_2',
-      '0_1', '0_2', '0_3', 'default_1'],
+      '0_1', '0_2', 'default_1'],
   },
   low: {
-    strategyDescription: 'Strict Stream Prioritization with Budgeted Base Layer',
+    strategyDescription: 'Save bandwidth by limiting the amount of layers',
     priorities: {
       default: ['fallback', 'SL0'],
       0: ['fallback', 'SL0'],
-      10: ['fallback', 'SL0', 'SL1', 'SL2'],
-      20: ['slideshow', 'SL0', 'SL1', 'SL2'],
-      30: ['fallback', 'SL0', 'SL1', 'SL2'],
+      10: ['fallback', 'SL0'],
+      20: ['slideshow', 'SL0'],
+      30: ['fallback', 'SL0'],
       40: ['fallback', 'SL0', 'SL1', 'SL2'],
     },
     strategy: ['40_0', '30_0', '20_0', '10_0', '0_0', 'default_0', // Base levels
@@ -33,6 +33,7 @@ bwDistributorConfig.strategyDefinitions = {
       '30_1',
       '20_1',
       '10_1',
+      '40_2', '40_3',
       '0_1', 'default_1'],
   },
 };
