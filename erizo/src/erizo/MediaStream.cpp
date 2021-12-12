3f94be6f4aefd9223e450e0711f9986bc1c1fd58
Javier Cervino <javier@minervaproject.com>
2021-12-13 00:35:36 +0100

    Avoid signed and unsigned comparison

--- erizo/src/erizo/MediaStream.cpp
+++ erizo/src/erizo/MediaStream.cpp
@@ -1139,7 +1139,7 @@ uint64_t MediaStream::getBitrateForHigherTemporalInSpatialLayer(int spatial_laye
 
 uint64_t MediaStream::getBitrateForLowerTemporalInSpatialLayer(int spatial_layer) {
   boost::mutex::scoped_lock lock(layer_bitrates_mutex_);
-  if ((int)layer_bitrates_.size() > spatial_layer && layer_bitrates_[spatial_layer].size() > 0) {
+  if (static_cast<int>(layer_bitrates_.size()) > spatial_layer && layer_bitrates_[spatial_layer].size() > 0) {
     return layer_bitrates_[spatial_layer][0];
   }
   return 0;
