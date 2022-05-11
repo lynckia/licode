19b1c4789f9378ae215207028974b7824195a132
Carlos Arriaga Prieto <47325335+Arri98@users.noreply.github.com>
2022-05-11 16:33:27 +0200

    Fix recording not stopping (#1812)

--- erizoAPI/ExternalOutput.cc
+++ erizoAPI/ExternalOutput.cc
@@ -23,7 +23,7 @@ class AsyncCloser : public Nan::AsyncWorker {
     ~AsyncCloser() {}
     void Execute() {
       external_output_->close();
-      while (external_output_->isRecording()) {
+      while (!external_output_->hasClosed()) {
         std::this_thread::sleep_for(std::chrono::milliseconds(100));
       }
     }
@@ -163,6 +163,7 @@ NAN_METHOD(ExternalOutput::init) {
   int r = me->init();
   info.GetReturnValue().Set(Nan::New(r));
 }
+
 
 
 
